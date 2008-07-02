5eb8cb6dc3006bfcb0c6217d10ed77be1c1fa4d5
Kenan Hodzic <buxus85@gmail.com>
2008-07-02 12:45:03 +0000

    Dodao raspored.php

---
+++ studentska/raspored.php
@@ -0,0 +1,26 @@
+<?
+function studentska_raspored ()
+	{
+		global $userid,$user_siteadmin,$user_studentska;
+
+		global $_lv_; // Potrebno za genform() iz libvedran
+
+
+		require("lib/manip.php"); // radi ispisa studenata sa predmeta
+
+
+		// Provjera privilegija
+
+		if (!$user_studentska && !$user_siteadmin) {
+			zamgerlog("nije studentska",3); // 3: error
+			biguglyerror("Pristup nije dozvoljen.");
+			return;
+		}
+
+		require_once "classes/admin_raspored.php";
+
+		$aRas = new adminRaspored;
+
+		return $aRas ->printajAdministracijuRasporeda();
+	}
+?>
