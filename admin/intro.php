fa1fd65fc159d55e6d421ac2d5d5c0d08e90960d
Vedran Ljubovic <vljubovic@smartnet.ba>
2008-09-05 11:45:11 +0000

    admin/intro: Dodan pogled 'Svi studenti'

--- admin/intro.php
+++ admin/intro.php
@@ -3,6 +3,7 @@
 // ADMIN/INTRO - pocetna stranica za admina - stablo predmeta
 
 // v3.9.1.0 (2008/02/26) + Novi modul, admin/intro
+// v3.9.1.1 (2008/09/01) + Dodan pogled "Svi studenti"
 
 
 
@@ -13,7 +14,15 @@ function admin_intro() {
 <p>Izaberite predmet koji želite administrirati:</p>
 <?
 	require("public/predmeti.php");
-	public_predmeti("nastavnik/predmet&c=N");
+
+if ($_REQUEST['grupe']) {
+	public_predmeti("saradnik/grupa");
+	?><p><a href="?sta=admin/intro">Pogled "Administracija predmeta"</a></p><?
+} else {
+	public_predmeti("nastavnik/predmet");
+	?><p><a href="?sta=admin/intro&grupe=1">Pogled "Svi studenti"</a></p><?
+}
+
 }
 
 ?>
