e217f161dc095e638b77ab0f37c7fd882d54f1b0
Vedran Ljubovic <vljubovic@smartnet.ba>
2009-02-10 10:45:37 +0000

    studentska/izvjestaji: Dodan overlay za prikaz izvjestaja, dodan link na izvjestaj "genijalci"

--- studentska/izvjestaji.php
+++ studentska/izvjestaji.php
@@ -6,6 +6,8 @@
 // v3.9.1.1 (2008/09/08) + Polje aktuelna u tabeli akademska_godina
 // v3.9.1.2 (2008/09/09) + Dodan izvjestaj "studenti kojima nedostaje..."
 // v3.9.1.3 (2008/09/23) + Dodana opcija "Svi studiji" i sortiranje po broju indeksa
+// v3.9.1.4 (2009/01/26) + Dodan overlay za prikaz izvjestaja
+// v3.9.1.5 (2009/02/07) + Dodan link za izvjestaj "genijalci"
 
 
 
@@ -24,23 +26,51 @@ if (!$user_studentska && !$user_siteadmin) {
 
 
 
+// Kada se klikne na generisanje izvještaja, biće zasivljen ekran i prikazan prozor koji
+// obavjestava da je u toku generisanje izvještaja.
+// Razlog: Nihada (among else) ima običaj da klika na link sve dok se stranica ne otvori,
+// što ne samo da nema efekta nego i opterećuje server
+
+
 ?>
 <p><h3>Studentska služba - Izvještaj o prolaznosti</h3></p>
 
-<?
 
+<script language="JavaScript">
+function izvjestaj() {
+	document.getElementById('prekrivac').style.display="inline";
+	document.getElementById('obavijest').style.display="inline";
+	document.getElementById('obavijest').style.top=document.height/2-25;
+	document.getElementById('obavijest').style.left=document.width/2-150;
 
+	//alert("Hello");
+	return true;
+}
+</script>
 
+<img src="/images/blur.gif" width="1" height="1" border="0"> <!-- preloading -->
 
-	?>
+<div id="prekrivac" name="prekrivac" style="display:none; position: absolute; left: 0px; top: 55px">
+<table width="1024" height="900" border="0" cellspacing="0" cellpadding="0"><tr><td background="/images/blur.gif" align="center" valign="center">
+&nbsp;
+</td></tr></table>
+</div>
+
+<div id="obavijest" name="obavijest" style="display:none; position: absolute; left: 0px; top: 55px">
+<table width="300" height="50" border="1" cellspacing="0" cellpadding="0"><tr><td align="center" valign="center" width="50"  bgcolor="#DDDDDD"><img src="/images/Animated-Hourglass.gif" width="38" height="38"></td><td align="center" valign="center" bgcolor="#DDDDDD">U toku je kreiranje izvještaja<br>Molimo sačekajte</td></tr></table>
+</div>
+
 
 <p>Najčešći izvještaji:
 <ul>
-<li><a href="?sta=izvjestaj/granicni&polozili=1">Spisak studenata koji su dali uslove za upis u sljedeći semestar</a></li>
-<li><a href="?sta=izvjestaj/granicni&parcijalni=1&predmet=1&akademska_godina=3">Granični slučajevi - po studiju</a></li>
-<li><a href="?sta=izvjestaj/granicni&parcijalni=1&predmet=1&sort=predmet&akademska_godina=3">Granični slučajevi - po predmetu</a></li>
-<li><a href="?sta=izvjestaj/granicni&predmet=1&akademska_godina=4">Studenti kojima fali 1 predmet - 2008/2009</a></li>
-<li><a href="?sta=izvjestaj/granicni&predmet=2&akademska_godina=4">Studenti kojima fale 2 predmeta - 2008/2009</a></li>
+<li><a href="?sta=izvjestaj/granicni&polozili=1" onclick="izvjestaj();">Spisak studenata koji su dali uslove za upis u sljedeći semestar</a></li>
+<li><a href="?sta=izvjestaj/granicni&parcijalni=1&predmet=1&akademska_godina=3" onclick="izvjestaj();">Granični slučajevi - po studiju</a></li>
+<li><a href="?sta=izvjestaj/granicni&parcijalni=1&predmet=1&sort=predmet&akademska_godina=3" onclick="izvjestaj();">Granični slučajevi - po predmetu</a></li>
+<li><a href="?sta=izvjestaj/granicni&predmet=1&akademska_godina=4" onclick="izvjestaj();">Studenti kojima fali 1 predmet - 2008/2009</a></li>
+<li><a href="?sta=izvjestaj/granicni&predmet=2&akademska_godina=4" onclick="izvjestaj();">Studenti kojima fale 2 predmeta - 2008/2009</a></li>
+<li><a href="?sta=izvjestaj/prolaznosttab" onclick="izvjestaj();">Tabelarni pregled prolaznosti</a></li>
+<li><a href="?sta=izvjestaj/pregled" onclick="izvjestaj();">Pregled upisanih studenata u školsku 2008/09 godinu</a></li>
+<li>Rang liste po prosjeku za MSc studij: <a href="?sta=izvjestaj/genijalci&studij=7">RI</a>, <a href="?sta=izvjestaj/genijalci&studij=8">AE</a>, <a href="?sta=izvjestaj/genijalci&studij=9">EE</a>, <a href="?sta=izvjestaj/genijalci&studij=10">TK</a></li>
 </ul></p>
 
 <hr/>
@@ -66,7 +96,7 @@ function setCheckedValue(radioObj, newValue) {
 }
 	</script>
 	<table width="500" border="0"><tr><td align="left">
-		<form action="index.php" method="GET" name="studijForm">
+		<form action="index.php" method="GET" name="studijForm" onsubmit="return izvjestaj();">
 		<input type="hidden" name="sta" value="izvjestaj/prolaznost">
 		Akademska godina: <select name="_lv_column_akademska_godina">
 		<?
