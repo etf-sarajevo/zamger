1efdf928ff8f353953e6dbb8335391365887a803
Haris Agic <dexter.ba@gmail.com>
2009-03-18 17:06:45 +0000

    Copied remotely

---
+++ lib/config.php
@@ -0,0 +1,43 @@
+<?
+
+// LIB/CONFIG - default konfiguracija za zamger
+
+// v3.9.1.0 (2008/02/12) + Pocetak
+// v3.9.1.1 (2008/09/05) + Dodane opcije: $conf_naziv_intitucije + skraceni oblik, $conf_site_url, $conf_promjena_sifre
+
+$conf_naziv_institucije = "Elektrotehnički fakultet Sarajevo";
+$conf_skr_naziv_institucije = "ETF";
+$conf_skr_naziv_institucije_genitiv = "ETFa";
+
+$conf_dbhost = "localhost";
+$conf_dbuser = "root";
+$conf_dbpass = "";
+$conf_dbdb = "zamger";
+
+$conf_site_url = "https://zamger.etf.unsa.ba";
+
+$conf_files_path = "/srv/httpd/zamger";
+$conf_script_path = "/srv/httpd/zamger/www";
+
+$conf_appname = "ZAMGER";
+$conf_appversion = "4.0 beta2";
+
+//$conf_system_auth = "ldap";
+$conf_system_auth = "table";
+
+// LDAP stuff:
+$conf_ldap_server = "localhost";
+$conf_ldap_domain = "@moja.domena.ba"; // string koji se dodaje na uid da bi se dobila email adresa
+// Vidjeti funkciju gen_ldap_uid() u lib/zamger.php!!!
+
+// URL za promjenu sifre (u slucaju LDAPa)
+$conf_promjena_sifre = "<a href=\"promjena-sifre.php\" target=\"_blank\">promjena šifre</a>";
+
+$conf_use_mysql_utf8 = true; // potreban mysql 5.0+
+
+$conf_debug=1; // razne debug poruke
+$_lv_["debug"]=1; // ovo je za libvedran
+
+
+
+?>
