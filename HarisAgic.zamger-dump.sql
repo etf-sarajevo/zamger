ae4911d1baefbc129171427bd040e6f245dda713
Haris Agic <dexter.ba@gmail.com>
2009-05-07 23:13:23 +0000

    Promjene na entitetima: izbrisan bl_clanak_slika, tim, osoba_tim, projekat_link i projekat_rss se vezu za projekat.

--- HarisAgic.zamger-dump.sql
+++ HarisAgic.zamger-dump.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 -- 
 -- Host: localhost
--- Generation Time: Apr 14, 2009 at 04:22 PM
+-- Generation Time: May 07, 2009 at 06:39 PM
 -- Server version: 5.0.24
 -- PHP Version: 5.1.6
 -- 
@@ -16,7 +16,8 @@
 -- Table structure for table `bb_post`
 -- 
 
-CREATE TABLE IF NOT EXISTS `bb_post` (
+DROP TABLE IF EXISTS `bb_post`;
+CREATE TABLE `bb_post` (
   `id` int(11) NOT NULL,
   `vrijeme` timestamp NOT NULL default CURRENT_TIMESTAMP,
   `osoba` int(11) NOT NULL,
@@ -30,7 +31,8 @@ CREATE TABLE IF NOT EXISTS `bb_post` (
 -- Table structure for table `bb_post_text`
 -- 
 
-CREATE TABLE IF NOT EXISTS `bb_post_text` (
+DROP TABLE IF EXISTS `bb_post_text`;
+CREATE TABLE `bb_post_text` (
   `post` int(11) NOT NULL,
   `naslov` varchar(200) collate utf8_slovenian_ci NOT NULL,
   `tekst` text collate utf8_slovenian_ci NOT NULL,
@@ -43,7 +45,8 @@ CREATE TABLE IF NOT EXISTS `bb_post_text` (
 -- Table structure for table `bb_tema`
 -- 
 
-CREATE TABLE IF NOT EXISTS `bb_tema` (
+DROP TABLE IF EXISTS `bb_tema`;
+CREATE TABLE `bb_tema` (
   `id` int(11) NOT NULL,
   `vrijeme` timestamp NOT NULL default CURRENT_TIMESTAMP,
   `naslov` varchar(200) collate utf8_slovenian_ci NOT NULL,
@@ -60,39 +63,29 @@ CREATE TABLE IF NOT EXISTS `bb_tema` (
 -- Table structure for table `bl_clanak`
 -- 
 
-CREATE TABLE IF NOT EXISTS `bl_clanak` (
+DROP TABLE IF EXISTS `bl_clanak`;
+CREATE TABLE `bl_clanak` (
   `id` int(11) NOT NULL,
   `naslov` varchar(200) collate utf8_slovenian_ci NOT NULL,
   `tekst` text collate utf8_slovenian_ci NOT NULL,
+  `slika` varchar(100) collate utf8_slovenian_ci NOT NULL,
   `vrijeme` timestamp NOT NULL default CURRENT_TIMESTAMP,
   `osoba` int(11) NOT NULL,
+  `projekat` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
 
 -- --------------------------------------------------------
 
 -- 
--- Table structure for table `bl_clanak_slika`
+-- Table structure for table `osoba_projekat`
 -- 
 
-CREATE TABLE IF NOT EXISTS `bl_clanak_slika` (
-  `id` int(11) NOT NULL,
-  `filename` varchar(100) collate utf8_slovenian_ci NOT NULL,
-  `vrijeme` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  `clanak` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
-
--- --------------------------------------------------------
-
--- 
--- Table structure for table `osoba_tim`
--- 
-
-CREATE TABLE IF NOT EXISTS `osoba_tim` (
+DROP TABLE IF EXISTS `osoba_projekat`;
+CREATE TABLE `osoba_projekat` (
   `student` int(11) NOT NULL,
-  `tim` int(11) NOT NULL,
-  PRIMARY KEY  (`student`,`tim`)
+  `projekat` int(11) NOT NULL,
+  PRIMARY KEY  (`student`,`projekat`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
 
 -- --------------------------------------------------------
@@ -101,12 +94,15 @@ CREATE TABLE IF NOT EXISTS `osoba_tim` (
 -- Table structure for table `predmet_parametri`
 -- 
 
-CREATE TABLE IF NOT EXISTS `predmet_parametri` (
+DROP TABLE IF EXISTS `predmet_parametri`;
+CREATE TABLE `predmet_parametri` (
   `predmet` int(11) NOT NULL,
   `min_timova` tinyint(3) NOT NULL,
   `max_timova` tinyint(3) NOT NULL,
   `min_clanova_tima` tinyint(3) NOT NULL,
-  `max_clanova_tima` tinyint(3) NOT NULL
+  `max_clanova_tima` tinyint(3) NOT NULL,
+  `zakljucani_projekti` tinyint(2) NOT NULL default '0',
+  PRIMARY KEY  (`predmet`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
 
 -- --------------------------------------------------------
@@ -115,10 +111,10 @@ CREATE TABLE IF NOT EXISTS `predmet_parametri` (
 -- Table structure for table `projekat`
 -- 
 
-CREATE TABLE IF NOT EXISTS `projekat` (
+DROP TABLE IF EXISTS `projekat`;
+CREATE TABLE `projekat` (
   `id` int(11) NOT NULL,
   `naziv` varchar(200) collate utf8_slovenian_ci NOT NULL,
-  `tim` int(11) NOT NULL,
   `predmet` int(11) NOT NULL,
   `opis` text collate utf8_slovenian_ci NOT NULL,
   `vrijeme` timestamp NOT NULL default CURRENT_TIMESTAMP,
@@ -131,7 +127,8 @@ CREATE TABLE IF NOT EXISTS `projekat` (
 -- Table structure for table `projekat_file`
 -- 
 
-CREATE TABLE IF NOT EXISTS `projekat_file` (
+DROP TABLE IF EXISTS `projekat_file`;
+CREATE TABLE `projekat_file` (
   `id` int(11) NOT NULL,
   `naslov` varchar(200) collate utf8_slovenian_ci NOT NULL,
   `filename` varchar(100) collate utf8_slovenian_ci NOT NULL,
@@ -149,7 +146,8 @@ CREATE TABLE IF NOT EXISTS `projekat_file` (
 -- Table structure for table `projekat_file_diff`
 -- 
 
-CREATE TABLE IF NOT EXISTS `projekat_file_diff` (
+DROP TABLE IF EXISTS `projekat_file_diff`;
+CREATE TABLE `projekat_file_diff` (
   `file` int(11) NOT NULL,
   `diff` text collate utf8_slovenian_ci NOT NULL,
   PRIMARY KEY  (`file`)
@@ -161,10 +159,12 @@ CREATE TABLE IF NOT EXISTS `projekat_file_diff` (
 -- Table structure for table `projekat_link`
 -- 
 
-CREATE TABLE IF NOT EXISTS `projekat_link` (
+DROP TABLE IF EXISTS `projekat_link`;
+CREATE TABLE `projekat_link` (
   `id` int(11) NOT NULL,
+  `naziv` varchar(200) collate utf8_slovenian_ci NOT NULL,
   `url` varchar(200) collate utf8_slovenian_ci NOT NULL,
-  `poredak` tinyint(4) NOT NULL,
+  `opis` text collate utf8_slovenian_ci NOT NULL,
   `projekat` int(11) NOT NULL,
   `osoba` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
@@ -176,23 +176,13 @@ CREATE TABLE IF NOT EXISTS `projekat_link` (
 -- Table structure for table `projekat_rss`
 -- 
 
-CREATE TABLE IF NOT EXISTS `projekat_rss` (
-  `id` int(11) NOT NULL,
-  `url` varchar(200) collate utf8_slovenian_ci NOT NULL,
-  `poredak` tinyint(4) NOT NULL,
-  `projekat` int(11) NOT NULL,
-  `osoba` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
-
--- --------------------------------------------------------
-
--- 
--- Table structure for table `tim`
--- 
-
-CREATE TABLE IF NOT EXISTS `tim` (
+DROP TABLE IF EXISTS `projekat_rss`;
+CREATE TABLE `projekat_rss` (
   `id` int(11) NOT NULL,
   `naziv` varchar(200) collate utf8_slovenian_ci NOT NULL,
+  `url` varchar(200) collate utf8_slovenian_ci NOT NULL,
+  `opis` text collate utf8_slovenian_ci NOT NULL,
+  `projekat` int(11) NOT NULL,
+  `osoba` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
