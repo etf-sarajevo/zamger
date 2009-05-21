38eb882f90496abec57d6bbcd45c235c0e49c34a
Haris Agic <dexter.ba@gmail.com>
2009-05-21 20:36:21 +0000

    ovo mi ne treba...

--- directoryCreation.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once("lib/config.php");
-	global $conf_files_path;
-
-	mkdir ("$conf_files_path/projekti",0777, true);	
-	mkdir ("$conf_files_path/projekti/clanci",0777, true);	
-	mkdir ("$conf_files_path/projekti/fajlovi",0777, true);	
-?>
