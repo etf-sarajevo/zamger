e1e86d70e8d0c0dc87ef3b9e769df412fb8539c9
Haris Agic <dexter.ba@gmail.com>
2009-05-21 20:04:11 +0000

    pomocna skripta za kreiranje direktorija projekti, projekti/clanci/projekti/fajlovi

---
+++ directoryCreation.php
@@ -0,0 +1,9 @@
+<?php
+	global $conf_files_path;
+	echo $conf_files_path;
+/*
+	mkdir ("$conf_files_path/projekti",0777, true);	
+	mkdir ("$conf_files_path/projekti/clanci",0777, true);	
+	mkdir ("$conf_files_path/projekti/fajlovi",0777, true);	
+*/
+?>
