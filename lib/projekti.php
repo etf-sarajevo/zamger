b1d6d7b70d89788aa17a73ff637c5bccb3bb87d2
Haris Agic <dexter.ba@gmail.com>
2009-06-12 18:45:21 +0000

    nova funkcija za brisanje direktorija koji nije prazan

--- lib/projekti.php
+++ lib/projekti.php
@@ -254,6 +254,41 @@ function filtered_output_string($string)
 	return nl2br(stripslashes($string));
 }
 
+
+function rmdir_recursive($dirname)
+{
+    // Sanity check
+    if (!file_exists($dirname)) 
+	{
+        return false;
+    }
+
+    // Simple delete for a file
+    if (is_file($dirname) || is_link($dirname))
+	{
+        return unlink($dirname);
+    }
+
+    // Loop through the folder
+    $dir = dir($dirname);
+    while (false !== $entry = $dir->read()) 
+	{
+        // Skip pointers
+        if ($entry == '.' || $entry == '..') 
+		{
+            continue;
+        }
+
+        // Recurse
+        rmdir_recursive($dirname . DIRECTORY_SEPARATOR . $entry);
+    }
+
+    // Clean up
+    $dir->close();
+    return rmdir($dirname);
+}
+
+
 /**********************
 projektneStrane
 START
