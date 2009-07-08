bde954e165540648acc49a8afcc6ff528ef4528a
Haris Agic <dexter.ba@gmail.com>
2009-07-08 02:13:36 +0000

    promjena imena tabela + akademska godina dodana

--- HarisAgic.zamger-dump.sql
+++ HarisAgic.zamger-dump.sql
@@ -1,20 +1,23 @@
--- phpMyAdmin SQL Dump
--- version 2.9.0.2
+﻿-- phpMyAdmin SQL Dump
+-- version 2.11.9.2
 -- http://www.phpmyadmin.net
--- 
+--
 -- Host: localhost
--- Generation Time: May 20, 2009 at 10:13 PM
--- Server version: 5.0.24
--- PHP Version: 5.1.6
--- 
--- Database: `zamger`
--- 
+-- Generation Time: Jul 07, 2009 at 10:11 PM
+-- Server version: 5.0.51
+-- PHP Version: 5.2.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `zamgerdemo`
+--
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Table structure for table `bb_post`
--- 
+--
 
 DROP TABLE IF EXISTS `bb_post`;
 CREATE TABLE `bb_post` (
@@ -28,9 +31,9 @@ CREATE TABLE `bb_post` (
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Table structure for table `bb_post_text`
--- 
+--
 
 DROP TABLE IF EXISTS `bb_post_text`;
 CREATE TABLE `bb_post_text` (
@@ -41,9 +44,9 @@ CREATE TABLE `bb_post_text` (
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Table structure for table `bb_tema`
--- 
+--
 
 DROP TABLE IF EXISTS `bb_tema`;
 CREATE TABLE `bb_tema` (
@@ -59,9 +62,9 @@ CREATE TABLE `bb_tema` (
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Table structure for table `bl_clanak`
--- 
+--
 
 DROP TABLE IF EXISTS `bl_clanak`;
 CREATE TABLE `bl_clanak` (
@@ -77,45 +80,34 @@ CREATE TABLE `bl_clanak` (
 
 -- --------------------------------------------------------
 
--- 
--- Table structure for table `osoba_projekat`
--- 
+--
+-- Table structure for table `predmet_projektni_parametri`
+--
 
-DROP TABLE IF EXISTS `osoba_projekat`;
-CREATE TABLE `osoba_projekat` (
-  `student` int(11) NOT NULL,
-  `projekat` int(11) NOT NULL,
-  PRIMARY KEY  (`student`,`projekat`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
-
--- --------------------------------------------------------
-
--- 
--- Table structure for table `predmet_parametri`
--- 
-
-DROP TABLE IF EXISTS `predmet_parametri`;
-CREATE TABLE `predmet_parametri` (
+DROP TABLE IF EXISTS `predmet_projektni_parametri`;
+CREATE TABLE `predmet_projektni_parametri` (
   `predmet` int(11) NOT NULL,
+  `akademska_godina` int(11) NOT NULL default '0',
   `min_timova` tinyint(3) NOT NULL,
   `max_timova` tinyint(3) NOT NULL,
   `min_clanova_tima` tinyint(3) NOT NULL,
   `max_clanova_tima` tinyint(3) NOT NULL,
   `zakljucani_projekti` tinyint(2) NOT NULL default '0',
-  PRIMARY KEY  (`predmet`)
+  PRIMARY KEY  (`predmet`,`akademska_godina`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Table structure for table `projekat`
--- 
+--
 
 DROP TABLE IF EXISTS `projekat`;
 CREATE TABLE `projekat` (
   `id` int(11) NOT NULL,
   `naziv` varchar(200) collate utf8_slovenian_ci NOT NULL,
   `predmet` int(11) NOT NULL,
+  `akademska_godina` int(11) NOT NULL default '0',
   `opis` text collate utf8_slovenian_ci NOT NULL,
   `vrijeme` timestamp NOT NULL default CURRENT_TIMESTAMP,
   PRIMARY KEY  (`id`)
@@ -123,9 +115,9 @@ CREATE TABLE `projekat` (
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Table structure for table `projekat_file`
--- 
+--
 
 DROP TABLE IF EXISTS `projekat_file`;
 CREATE TABLE `projekat_file` (
@@ -141,9 +133,9 @@ CREATE TABLE `projekat_file` (
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Table structure for table `projekat_file_diff`
--- 
+--
 
 DROP TABLE IF EXISTS `projekat_file_diff`;
 CREATE TABLE `projekat_file_diff` (
@@ -154,9 +146,9 @@ CREATE TABLE `projekat_file_diff` (
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Table structure for table `projekat_link`
--- 
+--
 
 DROP TABLE IF EXISTS `projekat_link`;
 CREATE TABLE `projekat_link` (
@@ -166,14 +158,15 @@ CREATE TABLE `projekat_link` (
   `opis` text collate utf8_slovenian_ci NOT NULL,
   `projekat` int(11) NOT NULL,
   `osoba` int(11) NOT NULL,
+  `vrijeme` timestamp NOT NULL default CURRENT_TIMESTAMP,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Table structure for table `projekat_rss`
--- 
+--
 
 DROP TABLE IF EXISTS `projekat_rss`;
 CREATE TABLE `projekat_rss` (
@@ -183,5 +176,19 @@ CREATE TABLE `projekat_rss` (
   `opis` text collate utf8_slovenian_ci NOT NULL,
   `projekat` int(11) NOT NULL,
   `osoba` int(11) NOT NULL,
+  `vrijeme` timestamp NOT NULL default CURRENT_TIMESTAMP,
   PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_projekat`
+--
+
+DROP TABLE IF EXISTS `student_projekat`;
+CREATE TABLE `student_projekat` (
+  `student` int(11) NOT NULL,
+  `projekat` int(11) NOT NULL,
+  PRIMARY KEY  (`student`,`projekat`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
