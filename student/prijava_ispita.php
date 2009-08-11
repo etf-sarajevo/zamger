737bfeb9b48e61f2d0c30b4883c3131c18a9df3b
Admir Heric <admir@personal.com>
2009-08-11 20:02:25 +0000

    student/prijava_ispita komentari: * koristim globalnu varijablu $userid umjesto parametra * dodana provjera da li je popunjen termin * iz tabele ispita izbaceni ispiti iz druge akademske godine i polozeni ispiti

--- student/prijava_ispita.php
+++ student/prijava_ispita.php
@@ -8,13 +8,13 @@ function student_prijava_ispita() {
 global $userid;
 
 
+
 //dio koda koji se izvrsava kad se klikne na "odjavi"
 if ($_GET["akcija"]=="odjavi")
 {
-    $uid = intval($_GET['user_id']);
 	$termin = intval($_GET['termin']);
-	if ($uid && $termin) {
-	$sqlDelete="DELETE FROM student_ispit_termin WHERE student=" .$uid . " AND ispit_termin=" . $termin . ";";
+	if ($termin) {
+	$sqlDelete="DELETE FROM student_ispit_termin WHERE student=" .$userid . " AND ispit_termin=" . $termin . ";";
 	myquery($sqlDelete);
 	}
 ?>
@@ -27,13 +27,22 @@ if ($_GET["akcija"]=="odjavi")
 //dio koda koji se izvrsava kad se klikne na "prijavi"
 if ($_GET["akcija"]=="prijavi")
 {
-	$uid = intval($_GET['user_id']);
 	$termin = intval($_GET['termin']);
-    if ($uid && $termin) {
-	$sqlInsert="INSERT INTO student_ispit_termin (student,ispit_termin) VALUES (" .
-	$uid . "," . $termin . ");";
-	myquery($sqlInsert);
-	}
+    if ($termin) {
+	//sljedeci dio koda ispituje da li je popunjen ispitni termin
+	$s1 = "SELECT count(*) FROM student_ispit_termin WHERE ispit_termin=$termin";
+	$s2 = "SELECT maxstudenata FROM ispit_termin WHERE id=$termin";
+	$q1 = myquery ($s1);
+	$q2 = myquery ($s2);
+	$temp1 = mysql_fetch_row($q1);
+	$temp2 = mysql_fetch_row($q2);
+	if($temp1[0]<$temp2[0])
+		{	
+		$sqlInsert="INSERT INTO student_ispit_termin (student,ispit_termin) VALUES (" .
+		$userid . "," . $termin . ");";
+		myquery($sqlInsert);
+		}
+    }
 ?>
 	<script language="JavaScript">
 		window.location="?sta=student/prijava_ispita";
@@ -44,12 +53,15 @@ if ($_GET["akcija"]=="prijavi")
 
 
 
-$s1="SELECT DISTINCT p.naziv, UNIX_TIMESTAMP(it.datumvrijeme), UNIX_TIMESTAMP(it.deadline), k.gui_naziv, it.id, it.maxstudenata
-             FROM ispit_termin as it, ispit as i, predmet as p, komponenta as k, osoba as o, student_predmet as sp, ponudakursa as pk
-		 WHERE it.ispit=i.id AND it.komponenta=k.id AND i.predmet=pk.id AND p.id=pk.predmet AND o.id=$userid AND o.id=sp.student AND sp.predmet=pk.id
+$s3="SELECT DISTINCT p.naziv, UNIX_TIMESTAMP(it.datumvrijeme), UNIX_TIMESTAMP(it.deadline), k.gui_naziv, it.id, it.maxstudenata, k.id
+             FROM ispit_termin as it, ispit as i, predmet as p, komponenta as k, osoba as o, student_predmet as sp, ponudakursa as pk, akademska_godina as ag
+		 WHERE it.ispit=i.id AND it.komponenta=k.id AND i.predmet=pk.id AND p.id=pk.predmet AND o.id=$userid AND o.id=sp.student AND sp.predmet=pk.id AND pk.akademska_godina=ag.id AND ag.aktuelna=1
                    AND it.id NOT IN (SELECT ispit_termin FROM student_ispit_termin WHERE student=$userid)";
+$s4="SELECT io.ocjena, i.komponenta FROM ispit as i, ispit_termin as it, ispitocjene as io WHERE io.student=$userid AND io.ispit=i.id AND it.ispit=i.id";
 
-$q1 = myquery($s1);
+$q3 = myquery($s3);
+$q4 = myquery($s4);
+
 
 ?>
 <br><br>
@@ -68,12 +80,22 @@ $q1 = myquery($s1);
 <?php
 $brojac=1;
 
-while ($ispit=mysql_fetch_row($q1)) {
+while ($ispit=mysql_fetch_row($q3)) {
 
+//sljedeci dio koda izbacuje iz tabele polozene ispite
+    $br=0;
+	while ($a=mysql_fetch_row($q4))
+	{
+		if($ispit[6]==$a[1] && $a[0]>=10 && $a[1]==1) $br++;
+		if($ispit[6]==$a[1] && $a[0]>=10 && $a[1]==2) $br++;
+		if($ispit[6]==$a[1] && $a[0]>=20 && $a[1]==3) $br++;
+		if($ispit[6]==$a[1] && $a[0]>=20 && $a[1]==4) $br++;
+	}
+	if($br!=0) continue;
 //sljedeci dio koda ispituje da li je popunjen ispitni termin
-	$s3 = "SELECT count(*) FROM student_ispit_termin WHERE ispit_termin=" . $ispit[4];
-	$q3 = myquery ($s3);
-	$temp = mysql_fetch_row($q3);
+	$s5 = "SELECT count(*) FROM student_ispit_termin WHERE ispit_termin=" . $ispit[4];
+	$q5 = myquery ($s5);
+	$temp = mysql_fetch_row($q5);
 	if($temp[0]==$ispit[5]) continue;
 
 //naredne 2 linije provjeravaju da li je istekao rok za prijavu ispita
@@ -93,8 +115,7 @@ while ($ispit=mysql_fetch_row($q1)) {
 
 	<td align="center"><?=$ispit[3];?></td>
 
-	<td align="center"><a href="?sta=student/prijava_ispita&akcija=prijavi&user_id=<?php echo
-	$userid;?>&termin=<?php echo $ispit[4];?> ">Prijavi</a></td>
+	<td align="center"><a href="?sta=student/prijava_ispita&akcija=prijavi&termin=<?php echo $ispit[4];?> ">Prijavi</a></td>
 	</tr>
 
 <?php
@@ -115,11 +136,11 @@ echo "<b>Prijavljeni ispiti:</b>";
 //slijedeci dio koda sluzi za tabelarni prikaz prijavljenih predmeta
 
 
-$s2="SELECT DISTINCT p.naziv, UNIX_TIMESTAMP(it.datumvrijeme), k.gui_naziv, it.id
+$s6="SELECT DISTINCT p.naziv, UNIX_TIMESTAMP(it.datumvrijeme), k.gui_naziv, it.id
              FROM ispit_termin as it, ispit as i, predmet as p, osoba as o, student_predmet as sp, komponenta k, ponudakursa as pk, student_ispit_termin as si
              WHERE it.ispit=i.id AND pk.id=i.predmet  AND p.id=pk.predmet AND it.komponenta=k.id AND o.id=$userid AND o.id=sp.student AND sp.predmet=pk.id AND si.student=$userid AND si.student=o.id AND si.ispit_termin=it.id";
 
-$q2 = myquery($s2);
+$q6 = myquery($s6);
 
 ?>
 <br><br>
@@ -135,7 +156,7 @@ $q2 = myquery($s2);
 <?php
 $brojac=1;
 
-while ($ispit=mysql_fetch_row($q2)) {
+while ($ispit=mysql_fetch_row($q6)) {
 	$mytime = time(); // Postavi vrijeme na trenutno
       if($mytime>$ispit[1]) continue;
 
@@ -147,8 +168,7 @@ while ($ispit=mysql_fetch_row($q2)) {
 
 	<td align="center"><?=$ispit[2];?></td>
 	</td>
-	<td align="center"><a href="?sta=student/prijava_ispita&akcija=odjavi&user_id=<?php echo
-	$userid;?>&termin=<?php echo $ispit[3];?> ">Odjavi</a></td>
+	<td align="center"><a href="?sta=student/prijava_ispita&akcija=odjavi&termin=<?php echo $ispit[3];?> ">Odjavi</a></td>
 	</tr>
 
 <?php
