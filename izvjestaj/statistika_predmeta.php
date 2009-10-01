175ba2bc7e2da147b8a97693f8beed81ba7adea1
Vedran Ljubovic <vljubovic@smartnet.ba>
2009-10-01 22:43:01 +0000

    izvjestaj/statistika_predmeta: Ponovo napisan radi boljih performansi i podrske za predmete koji nisu tipa ETF Bologna standard; dodana informacija o broju ponovaca i koji put ponavljaju predmet

--- izvjestaj/statistika_predmeta.php
+++ izvjestaj/statistika_predmeta.php
@@ -3,20 +3,14 @@
 // IZVJESTAJ/STATISTIKA_PREDMETA - neke sumarne statistike za dati predmet
 
 // v4.0.9.1 (2009/04/22) + Novi izvjestaj napravljen na osnovu koda iz izvjestaj/ispit
+// v4.0.9.2 (2009/09/05) + Optimizovan izvjestaj koristeci komponente
+// v4.0.9.3 (2009/09/22) + Dodan broj ponovaca
 
-
-
-// NAPOMENA: Sumarne statistike rade samo za predmete tipa "ETF Bologna standard", odnosno 
-// predmete koji imaju standardni I i II parcijalni i Integralni. IDovi komponenti su 
-// ukodirani. FIXME
-
-// Ovo je vrlo sporo :(
 
 
 function izvjestaj_statistika_predmeta() {
 
 global $userid,$user_nastavnik,$user_studentska,$user_siteadmin;
-
 
 
 ?>
@@ -50,12 +44,13 @@ if (!$user_studentska && !$user_siteadmin) {
 
 // Naziv predmeta, akademske godine
 
-$q10 = myquery("select naziv from predmet where id=$predmet");
+$q10 = myquery("select naziv, tippredmeta from predmet where id=$predmet");
 if (mysql_num_rows($q10)<1) {
 	biguglyerror("Nepoznat predmet");
 	zamgerlog ("nepoznat predmet $predmet", 3);
 	return;
 }
+$tippredmeta = mysql_result($q10,0,1);
 
 $q15 = myquery("select naziv from akademska_godina where id=$ag");
 if (mysql_num_rows($q15)<1) {
@@ -67,79 +62,137 @@ if (mysql_num_rows($q15)<1) {
 	<p>&nbsp;</p>
 	<h1><?=mysql_result($q10,0,0)?> <?=mysql_result($q15,0,0)?></h1>
 	<h3>Sumarna statistika za sve ispite</h3>
+	<p>(Detaljnije informacije možete dobiti koristeći Puni izvještaj)</p>
 <?
+
 
 
 
 // Tijelo izvjestaja
 
-$q30 = myquery("select count(*) from student_predmet as sp, ponudakursa as pk where sp.predmet=pk.id and pk.predmet=$predmet and pk.akademska_godina=$ag");
-$slusa_predmet = mysql_result($q30,0,0);
+
+// Osnovne statistike
+$q30 = myquery("select sp.student from student_predmet as sp, ponudakursa as pk where sp.predmet=pk.id and pk.predmet=$predmet and pk.akademska_godina=$ag");
+$slusa_predmet = mysql_num_rows($q30);
 
 $q40 = myquery("select id from ispit where predmet=$predmet and akademska_godina=$ag");
 $odrzano_ispita = mysql_num_rows($q40);
 
-if ($odrzano_ispita>0) {
+$upisano_puta=array();
+$maxput=0;
+$upisano_puta[0]=$upisano_puta[1]=$upisano_puta[3]=$upisano_puta[4]=$upisano_puta[5]=0;
+
+// Ako nije održan nijedan ispit, ipak je dobro da vidimo neke stastistike
+//if ($odrzano_ispita>0) {
+
+	// Spisak ispita
 	$ispiti=array();
 	while ($r40 = mysql_fetch_row($q40)) array_push($ispiti,$r40[0]);
 
-	$q50 = myquery("select count(*) from konacna_ocjena where predmet=$predmet and akademska_godina=$ag and ocjena>5");
-	$polozilo = mysql_result($q50,0,0);
+	// Spisak komponenti
+	$knazivi=$kprolaz=$ktip=$kpolozilo=array();
+	$q50 = myquery("select k.id, k.gui_naziv, k.prolaz, k.tipkomponente from komponenta as k, tippredmeta_komponenta as tpk where tpk.tippredmeta=$tippredmeta and tpk.komponenta=k.id");
+	while ($r50 = mysql_fetch_row($q50)) {
+		$knazivi[$r50[0]]=$r50[1]; // k.gui_naziv
+		$kprolaz[$r50[0]]=$r50[2]; // k.prolaz
+		$ktip[$r50[0]]=$r50[3]; // k.tipkomponente
+		$kpolozilo[$r50[0]]=0;
+	}
 
-	$q60 = myquery("select count(*) from student_predmet as sp, ponudakursa as pk where sp.predmet=pk.id and pk.predmet=$predmet and pk.akademska_godina=$ag and (select count(*) from ispit as i, ispitocjene as io where i.predmet=$predmet and i.akademska_godina=$ag and io.ispit=i.id and io.student=sp.student)=0");
-	$nisu_izlazili = mysql_result($q60,0,0);
+	$q55 = myquery("select count(*) from konacna_ocjena where predmet=$predmet and akademska_godina=$ag and ocjena>5");
+	$polozilo = mysql_result($q55,0,0);
+
+	// Prolazimo kroz studente
+	$uslov40=$uslov35=$uslov0=$nisu_izlazili=0;
+	while ($r30 = mysql_fetch_row($q30)) {
+		$student = $r30[0];
+
+		// Odredjujem ponudukursa
+		$q55 = myquery("select pk.id from ponudakursa as pk, student_predmet as sp where sp.student=$student and sp.predmet=pk.id and pk.predmet=$predmet and pk.akademska_godina=$ag");
+		$ponudakursa = mysql_result($q55,0,0);
+
+		// Koliko puta je slusao predmet?
+		$q58 = myquery("select count(*) from student_predmet as sp, ponudakursa as pk where sp.student=$student and sp.predmet=pk.id and pk.predmet=$predmet and pk.akademska_godina<$ag");
+		$puta = intval(mysql_result($q58,0,0));
+		if ($puta>$maxput) $maxput=$puta;
+		$upisano_puta[$puta]++;
+
+		// Komponente
+		$izasao=$pao=$sumbodovi=0;
+		foreach($ktip as $komponenta=>$tip) {
+			$q60 = myquery("select bodovi from komponentebodovi where student=$student and predmet=$ponudakursa and komponenta=$komponenta");
+			if (mysql_num_rows($q60)>0) {
+				$bodovi = mysql_result($q60,0,0);
+				if ($tip==1 || $tip==2) $izasao++;
+				if ($kprolaz[$komponenta]==0) 
+					$kpolozilo[$komponenta]++;
+				else if ($bodovi>=$kprolaz[$komponenta])
+					$kpolozilo[$komponenta]++;
+				else $pao++;
+
+				$sumbodovi += $bodovi;
+			}
+		}
+		
+		// Da li je zadovoljio uslove?
+		if ($pao==0 && $izasao>0) {
+			// FIXME: ovo će brojati studente koji su položili samo jedan parcijalni, a na drugi nisu nikad izašli
+			// Jedino rješenje je da se na nivou predmeta definiše šta je tačno uslov
+			if ($sumbodovi>=40) $uslov40++;
+			if ($sumbodovi>=35) $uslov35++;
+			$uslov0++;
+		}
+
+		// Da probamo ovako...
+		if ($izasao==0) $nisu_izlazili++;
+	}
+
 	$stvarno_slusa = $slusa_predmet-$nisu_izlazili;
+//}
 
-	// Ako predmet nije bologna standard, daljnje statistike nemaju smisla - FIXME
-	$q70 = myquery("select tpk.komponenta from tippredmeta_komponenta as tpk, predmet as p where p.id=$predmet and p.tippredmeta=tpk.tippredmeta");
-	$bologna=0;
-	while ($r70 = mysql_fetch_row($q70)) {
-		if ($r70[0]==1) $bologna++;
-		if ($r70[0]==2) $bologna++;
-		if ($r70[0]==3) $bologna++;
-	}
 
-	if ($bologna>=3) {
-		$q80 = myquery("select count(*) from student_predmet as sp, ponudakursa as pk where sp.predmet=pk.id and pk.predmet=$predmet and pk.akademska_godina=$ag and (select count(*) from ispitocjene as io, ispit as i where i.predmet=$predmet and i.akademska_godina=$ag and i.komponenta=1 and io.ispit=i.id and io.student=sp.student and io.ocjena>=10)>0");
-		$prvaparc = mysql_result($q80,0,0);
-
-		$q90 = myquery("select count(*) from student_predmet as sp, ponudakursa as pk where sp.predmet=pk.id and pk.predmet=$predmet and pk.akademska_godina=$ag and (select count(*) from ispitocjene as io, ispit as i where i.predmet=$predmet and i.akademska_godina=$ag and i.komponenta=2 and io.ispit=i.id and io.student=sp.student and io.ocjena>=10)>0");
-		$drugaparc = mysql_result($q90,0,0);
-
-		$q100 = myquery("select count(*) from student_predmet as sp, ponudakursa as pk where sp.predmet=pk.id and pk.predmet=$predmet and pk.akademska_godina=$ag and (select count(*) from ispitocjene as io, ispit as i where i.predmet=$predmet and i.akademska_godina=$ag and i.komponenta=3 and io.ispit=i.id and io.student=sp.student and io.ocjena>=20)>0");
-		$intparc = mysql_result($q100,0,0);
-
-		$q110 = myquery("select count(*) from student_predmet as sp, ponudakursa as pk where sp.predmet=pk.id and pk.predmet=$predmet and pk.akademska_godina=$ag and (select count(*) from ispitocjene as io, ispit as i where i.predmet=$predmet and i.akademska_godina=$ag and i.komponenta=1 and io.ispit=i.id and io.student=sp.student and io.ocjena>=10)>0 and (select count(*) from ispitocjene as io, ispit as i where i.predmet=$predmet and i.akademska_godina=$ag and i.komponenta=2 and io.ispit=i.id and io.student=sp.student and io.ocjena>=10)>0");
-		$objeparc = mysql_result($q110,0,0);
-
-		$zad_uslove = $intparc+$objeparc;
-	}
-}
 
 ?>
 <p>Ukupno upisalo predmet: <b><?=$slusa_predmet?></b> studenata.<br/>
+<ul>
 <?
+
+for ($i=0; $i<=$maxput; $i++) {
+	if ($upisano_puta[$i]==0) continue;
+	print "<li>".($i+1).". put: <b>".$upisano_puta[$i]."</b> studenata</li>\n";
+}
+
+print "</ul>\n";
 
 if ($odrzano_ispita==0) {
 	?>Nije održan nijedan ispit.</p><?
 	return;
-}
-else if ($bologna<3) {
-	?>
-	Nije izašlo ni na jedan ispit (pretpostavka je da ne slušaju predmet, biće isključeni iz daljnjih statistika): <b><?=$nisu_izlazili?></b> studenata.<br/>
-	Položilo (konačna ocjena 6 ili više): <b><?=$polozilo?></b> studenata (<b><?=procenat($polozilo,$stvarno_slusa)?></b>).<br/>
-	Predmet nije Bologna standard tako da daljnje statistike nisu dostupne.</p>
-	<?
-	return;
+
 } else {
 	?>
 	Nije izašlo ni na jedan ispit (pretpostavka je da ne slušaju predmet, biće isključeni iz daljnjih statistika): <b><?=$nisu_izlazili?></b> studenata.<br/>
 	Položilo (konačna ocjena 6 ili više): <b><?=$polozilo?></b> studenata (<b><?=procenat($polozilo,$stvarno_slusa)?></b>).<br/>
-	Zadovoljilo uslove za usmeni: <b><?=$zad_uslove?></b> studenata (<b><?=procenat($zad_uslove,$stvarno_slusa)?></b>).<br/><br/>
-	Položilo I parcijalni ispit: <b><?=$prvaparc?></b> studenata  (<b><?=procenat($prvaparc,$stvarno_slusa)?></b>).<br/>
-	Položilo II parcijalni ispit: <b><?=$drugaparc?></b> studenata  (<b><?=procenat($drugaparc,$stvarno_slusa)?></b>).<br/>
-	Položilo oba parcijalna ispita: <b><?=$objeparc?></b> studenata  (<b><?=procenat($objeparc,$stvarno_slusa)?></b>).<br/>
-	Položilo ispit integralno: <b><?=$intparc?></b> studenata  (<b><?=procenat($intparc,$stvarno_slusa)?></b>).</p>
+	Zadovoljilo uslove za usmeni *:<br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;svi ispiti i min. 40 bodova: <b><?=$uslov40?></b> studenata (<b><?=procenat($uslov40,$stvarno_slusa)?></b>).<br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;svi ispiti i min. 35 bodova: <b><?=$uslov35?></b> studenata (<b><?=procenat($uslov35,$stvarno_slusa)?></b>).<br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;svi ispiti: <b><?=$uslov0?></b> studenata (<b><?=procenat($uslov0,$stvarno_slusa)?></b>).<br/>
+	<br/>
+	<?
+
+	// Komponente
+	foreach ($ktip as $komponenta=>$tip) {
+		if ($kpolozilo[$komponenta]==0) continue; // ova komponenta nije u funkciji
+		if ($tip==1 || $tip==2) {
+			?>
+			Položilo <?=$knazivi[$komponenta]?> ispit: <b><?=$kpolozilo[$komponenta]?></b> studenata  (<b><?=procenat($kpolozilo[$komponenta],$stvarno_slusa)?></b>).<br/>
+			<?
+		}
+	}
+
+	?>
+	</p>
+
+	<p>* - Napomena: pod "uslov za usmeni" misli se na uobičajenu šemu dva parcijalna ispita + jedan integralni ispit.</p>
 	<?
 	return;
 }
