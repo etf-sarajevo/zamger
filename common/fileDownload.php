4a80cce7d72f2fe5f69a7adcf4b07ca88c0aee57
Vedran Ljubovic <vljubovic@smartnet.ba>
2009-10-23 14:07:10 +0000

    common/filedownload: Osoba moze biti istovremeno nastavnik na jednom predmetu a student na drugom (demonstratori)

--- common/fileDownload.php
+++ common/fileDownload.php
@@ -2,43 +2,20 @@
 // COMMON/FILEDOWNLOAD - download fajlova na projektima
 function common_fileDownload()
 {
+	require_once("lib/projekti.php");
+
 	global $userid, $user_nastavnik, $user_student, $conf_files_path, $user_siteadmin;
 	$predmet 	= intval($_REQUEST['predmet']);
 	$ag		 	= intval($_REQUEST['ag']);
 	$projekat 	= intval($_REQUEST['projekat']);
 	$id = intval($_REQUEST['id']); //file ID
 	
-	if ($predmet <=0 || $projekat <=0 || $ag <=0)
-	{
-		//hijack attempt?
-		zamgerlog("korisnik u$userid pokusao pristupiti modulu common/fileDownload sa ID predmeta  ili ID projekta ili ag koji nije integer ili je <=0", 3);		
+	$q10 = myquery("select admin from nastavnik_predmet where nastavnik=$userid and predmet=$predmet and akademska_godina=$ag");
+	$actualProject = getActualProjectForUserInPredmet($userid, $predmet, $ag);
+	if ((mysql_num_rows($q10)<1 || mysql_result($q10,0,0)<1) && ($actualProject[id] != $projekat)) {
+		zamgerlog("nije ni student ni nastavnik (projekat $projekat, predmet pp$predmet)",3);
+		biguglyerror("Nemate pravo ulaska u ovu grupu!");
 		return;
-	}
-	
-	if ($user_nastavnik && !$user_siteadmin)
-	{
-		$q10 = myquery("select admin from nastavnik_predmet where nastavnik=$userid and predmet=$predmet and akademska_godina=$ag");
-		if (mysql_num_rows($q10)<1 || mysql_result($q10,0,0)<1) {
-			zamgerlog("common/projektneStrane privilegije (predmet pp$predmet)",3);
-			biguglyerror("Nemate pravo ulaska u ovu grupu!");
-			return;
-		} 	
-	}
-	
-	require_once("lib/projekti.php");
-
-
-	if ($user_student && !$user_siteadmin)
-	{
-		$actualProject = getActualProjectForUserInPredmet($userid, $predmet, $ag);
-		if ($actualProject[id] != $projekat)
-		{
-			//user is not in this project in this predmet...hijack attempt?
-			zamgerlog("korisnik u$userid pokusao pristupiti modulu common/fileDownload i projektu na kojem nije prijavljen ID=$projekat na predmetu p$predmet", 3);				
-			biguglyerror("Nemate pravo ulaska u ovu grupu!");
-			return;	
-		}
-		
 	}
 	
 	$entry = getFile($id);
