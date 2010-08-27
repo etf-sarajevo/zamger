954b2691285dfc57536a05d2932a74321c35bb55
Jasmin Krcalo <jk14888@etf.unsa.ba>
2010-08-27 13:49:15 +0000

    Proba

--- student/pdf.php
+++ student/pdf.php
@@ -123,7 +123,7 @@ class MYPDF extends TCPDF {
 	$this->Ln();
 	$this->SetX(80);
 	
-	$this->Cell(50,5,'ELEKTROTEHNIČKI FAKULTET','UTF-8',0,0,'C');
+	$this->Cell(50,5,'ELEKTROTEHNIČKI FAKULTET',0,0,'C');
 	$this->Ln();
 	$this->Cell(190,5,'','B',0,'C');
 	$this->Ln();
