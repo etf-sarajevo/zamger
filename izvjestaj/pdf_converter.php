8133bc8ab2c02ddb8a55600a57dbe1541558caef
Jasmin Krcalo <jk14888@etf.unsa.ba>
2010-08-31 15:41:01 +0000

    dodana skripta izvjestaj/pdf_converter.php koja ce omoguciti kreiranje bilo kojeg izvjestaja u pdf.

---
+++ izvjestaj/pdf_converter.php
@@ -0,0 +1,79 @@
+<?
+  
+function ob_file_callback($buffer)
+{
+  global $sadrzaj_bafera_za_pdf;
+  $sadrzaj_bafera_za_pdf=$buffer;
+
+}
+
+
+function izvjestaj_pdf_converter() {
+global $string_pdf,$string,$sadrzaj_bafera_za_pdf;
+
+ob_start('ob_file_callback');
+$koji = my_escape($_REQUEST['koji_izvjestaj']);
+$staf = str_replace("/","_",$koji);
+
+include("$koji.php");//ovdje ga ukljucujem
+eval("$staf();");
+ob_end_clean();
+
+require_once('lib/tcpdf/tcpdf.php');
+
+class MYPDF extends TCPDF {
+
+	//Page header
+	public function Header() {
+	
+	$this->SetMargins(10,35,25,true);	
+	$this->Image("images/etf-100x100.png",10,8,20);
+	$this->Image("images/unsa.png",180,8,20);
+	
+        $this->SetFont("DejaVu Sans",'',10);
+	$this->SetY(15);
+	$this->SetX(80);
+	$this->Cell(50,5,'UNIVERZITET U SARAJEVU',0,0,'C');
+	$this->Ln();
+	$this->SetX(80);
+	
+	$this->Cell(50,5,'ELEKTROTEHNIČKI FAKULTET',0,0,'C');
+	$this->Ln();
+	$this->Cell(190,5,'','B',0,'C');
+	$this->Ln();
+	$this->Ln();
+	}
+
+	// Page footer
+	public function Footer() {
+		
+	if ($this->PageNo() > 1) {
+		//Position at 1.5 cm from bottom
+		$this->SetY(-15);
+		//Arial italic 8
+		$this->SetFont('DejaVu Sans ','I',8);
+		//Text color in gray
+		$this->SetTextColor(128);
+		//Page number
+		$this->Cell(0,10,'Stranica '.$this->PageNo(),0,0,'C');
+	}
+	}
+}
+
+// Prva stranica
+$pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+$pdf->AddFont("DejaVuSans","","DejaVuSans.php");
+$pdf->AddFont("DejaVuSans","","DejaVuSans-Bold.php");
+$pdf->SetFont('DejaVuSans','',8);
+$pdf->AddPage();
+
+
+$sadrzaj_bafera_za_pdf = str_replace("\t","        ",$sadrzaj_bafera_za_pdf);
+
+$pdf->writeHTMLCell($w=0, $h=0, $x='', $y='', $sadrzaj_bafera_za_pdf, $border=0, $ln=1, $fill=0, $reseth=true, $align='center', $autopadding=true);
+
+$pdf->Output('Izjestaj.pdf', 'I');
+}
+
+?>
