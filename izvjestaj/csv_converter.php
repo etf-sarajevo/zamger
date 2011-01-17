0330cb4bf396ea4c75fd4b9aa62c4a10a59e48fb
Vedran Ljubovic <vljubovic@smartnet.ba>
2011-01-17 21:43:08 +0000

    Dodana kontrola prava pristupa u CSV i PDF konverter, puno detalja i posebnih slucajeva u CSV konverteru

--- izvjestaj/csv_converter.php
+++ izvjestaj/csv_converter.php
@@ -1,39 +1,137 @@
 <?
   
-function ob_file_callback($buffer)
-{
-  global $sadrzaj_bafera_za_csv;
-  $sadrzaj_bafera_za_csv=$buffer;
- 
+function ob_file_callback($buffer) {
+	global $sadrzaj_bafera_za_csv;
+	$sadrzaj_bafera_za_csv=$buffer;
 }
 
 
 function izvjestaj_csv_converter() {
-global $sadrzaj_bafera_za_csv,$conf_files_path;
+	global $sadrzaj_bafera_za_csv,$conf_files_path, $registry;
+	global $user_student, $user_nastavnik, $user_studentska, $user_siteadmin;
 
-ob_start('ob_file_callback');
-$koji = my_escape($_REQUEST['koji_izvjestaj']);
-$staf = str_replace("/","_",$koji);
+	$separator = ";"; // treba biti konfigurabilno
 
-include("$koji.php");//ovdje ga ukljucujem
-eval("$staf();");
-ob_end_clean();
+	ob_start('ob_file_callback');
+	$koji = my_escape($_REQUEST['koji_izvjestaj']);
+	$staf = str_replace("/","_",$koji);
 
-//Brisemo tagove iz teksta
-$sadrzaj_bafera_za_csv=str_replace("</tr>","\n",$sadrzaj_bafera_za_csv);
-$sadrzaj_bafera_za_csv=str_replace("</td>",";",$sadrzaj_bafera_za_csv);
-$sadrzaj_bafera_za_csv=strip_tags($sadrzaj_bafera_za_csv);
-$sadrzaj_bafera_za_csv=str_replace("&nbsp","",$sadrzaj_bafera_za_csv);
+	$found=false;
+	foreach ($registry as $r) {
+		if ($r[0] == $koji) {
+			if (strstr($r[3],"P") || (strstr($r[3],"S") && $user_student) || (strstr($r[3],"N") && $user_nastavnik) || (strstr($r[3],"B") && $user_studentska) || (strstr($r[3],"A") && $user_siteadmin)) {
+				$found=true;
+			} else {
+				zamgerlog ("csv_converter pristup nedozvoljenom modulu $koji", 3);
+				niceerror("Pristup nedozvoljenom modulu");
+				return;
+			}
+			break;
+		}
+	}
+	if ($found===false) {
+		zamgerlog ("csv_converter nepostojeći modul $koji", 3);
+		niceerror("Pristup nepostojećem modulu");
+		return;
+	}
 
-header("Content-Disposition: attachment; filename=".$koji.".csv");
-header("Content-Type: text/csv");
+
+	include("$koji.php");//ovdje ga ukljucujem
+	eval("$staf();");
+	ob_end_clean();
+
+	// Neke uobičajene tagove za novi red unutar polja tabele pretvaramo u space
+	// a van polja tabele u novi red
+	$sadrzaj_bafera_za_csv = preg_replace("/(\<td.*?\>.*?)\<br.*?\>(.*?\<\/td\>)/",'$1 $2',$sadrzaj_bafera_za_csv);
+	$sadrzaj_bafera_za_csv = preg_replace("/(\<td.*?\>.*?)\<\/p\>(.*?\<\/td\>)/",'$1 $2',$sadrzaj_bafera_za_csv);
+
+	// Ukidamo nove redove i spajamo whitespace
+	$sadrzaj_bafera_za_csv = str_replace("\n","",$sadrzaj_bafera_za_csv);
+	$sadrzaj_bafera_za_csv = str_replace("&nbsp;"," ",$sadrzaj_bafera_za_csv);
+	$sadrzaj_bafera_za_csv = preg_replace("/\s+/"," ",$sadrzaj_bafera_za_csv);
+
+	// Pretvaramo uobičajene tagove za novi red u \n
+	$sadrzaj_bafera_za_csv = preg_replace("/\<br.*?\>/","\n",$sadrzaj_bafera_za_csv);
+	$sadrzaj_bafera_za_csv = str_replace("</p>","\n",$sadrzaj_bafera_za_csv);
+	$sadrzaj_bafera_za_csv = preg_replace("/\<\/h[123]\>/","\n",$sadrzaj_bafera_za_csv);
+
+	// Na sve ćelije tabele dodajemo separator
+	// Zbog performansi unutar ove petlje ćemo obraditi razne posebne slučajeve
+	while (preg_match("/(\<td.*?\>)(.*?)(\<\/td\>)/", $sadrzaj_bafera_za_csv, $matches)) {
+		$sadrzaj = $matches[2];
+
+		// Ukidamo početni whitespace
+		while (substr($sadrzaj,0,1)==" ") $sadrzaj = substr($sadrzaj,1);
+
+		// Ako je separator tačka-zarez, decimale se razdvajaju zarezom
+		if ($separator == ";" && $result = strstr($sadrzaj,".")) {
+			while ($result) {
+				$res2 = ",".substr($result,1);
+				if (ord(substr($result, 1, 1)) >= ord('1') && ord(substr($result, 1, 1)) <= ord('9'))
+					$sadrzaj = str_replace ($result, $res2, $sadrzaj);
+				$result = strstr(substr($result,1), ".");
+			}
+		}
+			
+		// Ako se u sadržaju javlja separator, stavljamo pod navodnike
+		if ($result = strstr($sadrzaj,$separator)) {
+			// Ako je separator zarez, za decimale koristimo tačku
+			if ($separator == ",") {
+				$res2 = ".".substr($result,1);
+				if (ord(substr($result, 1, 1)) >= ord('0') && ord(substr($result, 1, 1)) <= ord('9'))
+					$sadrzaj = str_replace ($result, $res2, $sadrzaj);
+			}
+
+			// Ako i dalje sadrži separator stavljamo ga pod navodnike
+			if (strstr($sadrzaj,$separator)) {
+				// Prethodno sve navodnike u stringu dupliramo
+				$sadrzaj = str_replace('"', '""', $sadrzaj);
+				$sadrzaj = '"'.$sadrzaj.'"';
+			}
+		}
+
+		// Dodajemo separator na kraj
+		$sadrzaj = $sadrzaj.$separator;
+
+		// Za colspan dodajemo još separatora
+		if ($result = strstr($matches[1], "colspan=")) {
+			$i=8;
+			$broj = "";
+			do {
+				$slovo = substr($result, $i, 1);
+				$broj .= $slovo;
+				if ($broj=='"') $broj="";
+				else if ($slovo=='"' || $slovo==' ') break;
+				$i++;
+			} while (true);
+			for ($i=1; $i<$broj; $i++)
+				$sadrzaj = $sadrzaj.$separator;
+		}
+		// TODO može li se rowspan nekako riješiti? morao bi se pratiti <tr>
+
+		// Ubacujemo novi sadržaj
+		$sadrzaj_bafera_za_csv = str_replace($matches[0], $sadrzaj, $sadrzaj_bafera_za_csv);
+	}
+
+	// Pretvaramo </tr> u novi red a </td> u separator
+	$sadrzaj_bafera_za_csv = str_replace("</tr>","\n",$sadrzaj_bafera_za_csv);
+	$sadrzaj_bafera_za_csv = str_replace("</td>",$separator,$sadrzaj_bafera_za_csv);
+
+	// Ostale tagove ukidamo
+	$sadrzaj_bafera_za_csv = strip_tags($sadrzaj_bafera_za_csv);
+
+	// Čistimo whitespace na početku reda / ćelije
+	$sadrzaj_bafera_za_csv = str_replace("\n ","\n",$sadrzaj_bafera_za_csv);
+	$sadrzaj_bafera_za_csv = str_replace("$separator ","$separator",$sadrzaj_bafera_za_csv);
 
 
-header("Pragma: dummy=bogus"); 
-header("Cache-Control: private");
-print $sadrzaj_bafera_za_csv;
+	header("Content-Disposition: attachment; filename=".$staf.".csv");
+	header("Content-Type: text/csv");
 
+	header("Pragma: dummy=bogus"); 
+	header("Cache-Control: private");
 
+	print $sadrzaj_bafera_za_csv;
 }
 
 ?>
