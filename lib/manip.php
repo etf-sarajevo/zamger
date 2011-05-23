49c59ec0319a118d3b0de880d73e2ecd81559d2b
Vedran Ljubovic <vljubovic@smartnet.ba>
2011-05-23 14:04:35 +0000

    lib/manip: Ako se funkciji update_komponente posalje komponenta 0, stari podaci o bodovima za komponente trebaju biti obrisani

--- lib/manip.php
+++ lib/manip.php
@@ -366,6 +366,9 @@ function mass_input($ispis) {
 function update_komponente($student,$predmet,$komponenta=0) {
 	// Ako nije navedena komponenta, racunaju se sve komponente
 
+	// Brišemo podatke trenutno u bazi ako je komponenta=0
+	$q5 = myquery("delete from komponentebodovi where student=$student and predmet=$predmet");
+
 	// Glavni upit - spisak komponenti
 	$dodaj="";
 	if ($komponenta!=0) $dodaj="and k.id=$komponenta";
