1fb42d327fe36b6e1955f41bd4804a0e8a34316d
Sedin Sabljica <ss14924@etf.unsa.ba>
2011-08-31 19:40:45 +0000

    Modifikovan izvjestaj za prikaz dodatnog statusa prisustva NP.

--- izvjestaj/predmet.php
+++ izvjestaj/predmet.php
@@ -427,10 +427,12 @@ foreach ($spisak_grupa as $grupa_id => $grupa_naziv) {
 					$q200 = mysql_query("select prisutan,plus_minus from prisustvo where student=$stud_id and cas=$cid");
 					if (mysql_num_rows($q200)>0) {
 						if (mysql_result($q200,0,0) == 1) { 
-							$ispis .= "<td bgcolor=\"#CCFFCC\" align=\"center\">DA</td>\n";
-						} else { 
+							$ispis .= "<td bgcolor=\"#FFE303\" align=\"center\">NP</td>\n";
+						} else if(mysql_result($q200,0,0) == 2){ 
 							$ispis .= "<td bgcolor=\"#FFCCCC\" align=\"center\">NE</td>\n";
 							$odsustvo++;
+						} else if(mysql_result($q200,0,0) == 3){
+							$ispis .= "<td bgcolor=\"#CCFFCC\" align=\"center\">DA</td>\n";
 						}
 						//$ocj = mysql_result($r4,0,1);
 					} else {
