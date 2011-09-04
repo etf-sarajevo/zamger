e35f74400cc162285f1ef18ba01fabd40b09029f
Enes Ljevakovic <el15205@etf.unsa.ba>
2011-09-04 12:05:46 +0000

    Dump baze podataka, i po potrebi samo dodatka (tabele "dogadjaji")

---
+++ dogadjaji.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Sep 04, 2011 at 11:59 AM
+-- Server version: 5.5.8
+-- PHP Version: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `zamger`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dogadjaji`
+--
+
+CREATE TABLE IF NOT EXISTS `dogadjaji` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `naziv` varchar(45) COLLATE utf8_slovenian_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `dogadjaji`
+--
+
