b56194055f5d6c2203ad1eedcd5687099fce4faf
Adnan Efendic <ae15365@etf.unsa.ba>
2012-09-09 21:26:52 +0000

    Updateovani moduli norma_plata.php i plata.php da reflektuju najnoviji koncept kolicine predavanja/vjezbi/tutorijala

--- common/profil/plata.php
+++ common/profil/plata.php
@@ -20,6 +20,8 @@ echo "<h2>Detalji o plati korisnika $ime $prezime</h2><br>";
 $fk_naucnonastavno_zvanje = intval(mysql_result(myquery("SELECT fk_naucnonastavno_zvanje FROM izbor WHERE fk_osoba = $osoba and CONCAT(year(datum_izbora),'/',year(datum_isteka)) = (SELECT naziv FROM akademska_godina WHERE aktuelna = 1)"), 0, 0));
 $fk_naucnonastavno_zvanje2 = intval(mysql_result(myquery("SELECT fk_naucnonastavno_zvanje FROM izbor WHERE fk_osoba = $osoba ORDER BY year(datum_isteka) DESC"), 0, 0));
 	if($fk_naucnonastavno_zvanje == 1 || $fk_naucnonastavno_zvanje == 2 || $fk_naucnonastavno_zvanje == 6){ //u slucaju redovnog profesora, vandrednog profesora ili profesora emeritusa
+		/*
+		stari nacin, ne koristi se vise
 		$broj_vjezbi_tutorijala = intval(mysql_result(myquery("SELECT  SUM( k.sati_vjezbi ) + SUM( k.sati_tutorijala ) AS ukupno_casova
 			FROM predmet AS p
 			JOIN labgrupa AS l ON p.id = l.predmet
@@ -35,21 +37,44 @@ $fk_naucnonastavno_zvanje2 = intval(mysql_result(myquery("SELECT fk_naucnonastav
 			JOIN angazman AS a ON a.osoba = o.id AND a.predmet = p.id
 			WHERE o.id = $osoba
 			GROUP BY a.osoba"), 0 ,0));
+		*/
+		$broj_predavanja = myquery("SELECT  p.id, l.naziv, tip AS tip_grupe
+			FROM predmet AS p
+			JOIN labgrupa AS l ON p.id = l.predmet
+			JOIN angazman AS a ON p.id = a.predmet
+			JOIN osoba AS o ON o.id = a.osoba
+			JOIN nastavnik_labgrupa as nl ON nl.osoba_id = o.id
+			JOIN akademska_godina AS ag ON ag.id = a.akademska_godina
+			WHERE ag.aktuelna = 1 AND l.id = nl.labgrupa_id AND o.id = $osoba_URL");//, 0 , 0));
+		$broj_predmeta = 0;
+		$broj_vjezbi_tutorijala = 0;
+		while($row = mysql_fetch_row($broj_predavanja)){
+			$pred_id = $row[0];
+			$lab_ime = $row[1];
+			$tip = $row[2];
+			switch($tip){
+				case "predavanja":
+					$broj_predmeta = $broj_predmeta + 1;//intval(mysql_result(myquery("SELECT sati_predavanja FROM predmet WHERE id = $pred_id"),0,0));
+					break;
+				case "vjezbe":
+					$broj_vjezbi_tutorijala = $broj_vjezbi_tutorijala + intval(mysql_result(myquery("SELECT sati_vjezbi FROM predmet WHERE id = $pred_id"),0,0));
+					break;
+				case "tutorijali":
+					$broj_vjezbi_tutorijala = $broj_vjezbi_tutorijala + intval(mysql_result(myquery("SELECT sati_tutorijala FROM predmet WHERE id = $pred_id"),0,0));
+					break;
+				case "vjezbe+tutorijali":
+					$broj_vjezbi_tutorijala = $broj_vjezbi_tutorijala + 2 * intval(mysql_result(myquery("SELECT sati_tutorijala FROM predmet WHERE id = $pred_id"),0,0));
+					break;
+			}
+		}
 		$predmet_norma = $broj_predmeta / 3;
 		$vjezbe_tutorijali_norma = $broj_vjezbi_tutorijala / 12;
 		$norma = $predmet_norma + $vjezbe_tutorijali_norma;
 	}
 	else if($fk_naucnonastavno_zvanje == 4 || $fk_naucnonastavno_zvanje == 5){ //u slucaju asistenta ili viseg asistenta
-		$broj_predavanja = intval(mysql_result(myquery("SELECT  SUM( k.sati_predavanja ) AS ukupno_casova
-			FROM predmet AS p
-			JOIN labgrupa AS l ON p.id = l.predmet
-			JOIN angazman AS a ON p.id = a.predmet
-			JOIN osoba AS o ON o.id = a.osoba
-			JOIN kolicina_predavanja as k ON k.osoba_id = o.id
-			JOIN akademska_godina AS ag ON ag.id = a.akademska_godina
-			WHERE ag.aktuelna = 1 AND l.id = k.labgrupa_id AND o.id = $osoba
-			GROUP BY ime"), 0, 0));
-		$broj_vjezbi_tutorijala = intval(mysql_result(myquery("SELECT  SUM( k.sati_predavanja ) + SUM( k.sati_vjezbi ) + SUM( k.sati_tutorijala ) AS ukupno_casova
+		/*
+		stari nacin, ne koristi se vise
+		$broj_vjezbi_tutorijala = intval(mysql_result(myquery("SELECT  SUM( k.sati_vjezbi ) + SUM( k.sati_tutorijala ) AS ukupno_casova
 			FROM predmet AS p
 			JOIN labgrupa AS l ON p.id = l.predmet
 			JOIN angazman AS a ON p.id = a.predmet
@@ -58,7 +83,43 @@ $fk_naucnonastavno_zvanje2 = intval(mysql_result(myquery("SELECT fk_naucnonastav
 			JOIN akademska_godina AS ag ON ag.id = a.akademska_godina
 			WHERE ag.aktuelna = 1 AND l.id = k.labgrupa_id AND o.id = $osoba
 			GROUP BY ime"), 0 , 0));
-		$predavanja_norma = $broj_predavanja / 12;
+		$broj_predmeta = intval(mysql_result(myquery("SELECT COUNT( p.id ) 
+			FROM osoba AS o
+			JOIN predmet AS p
+			JOIN angazman AS a ON a.osoba = o.id AND a.predmet = p.id
+			WHERE o.id = $osoba
+			GROUP BY a.osoba"), 0 ,0));
+		*/
+		$broj_predavanja = myquery("SELECT  p.id, l.naziv, tip AS tip_grupe
+			FROM predmet AS p
+			JOIN labgrupa AS l ON p.id = l.predmet
+			JOIN angazman AS a ON p.id = a.predmet
+			JOIN osoba AS o ON o.id = a.osoba
+			JOIN nastavnik_labgrupa as nl ON nl.osoba_id = o.id
+			JOIN akademska_godina AS ag ON ag.id = a.akademska_godina
+			WHERE ag.aktuelna = 1 AND l.id = nl.labgrupa_id AND o.id = $osoba_URL");//, 0 , 0));
+		$broj_predmeta = 0;
+		$broj_vjezbi_tutorijala = 0;
+		while($row = mysql_fetch_row($broj_predavanja)){
+			$pred_id = $row[0];
+			$lab_ime = $row[1];
+			$tip = $row[2];
+			switch($tip){
+				case "predavanja":
+					$broj_predmeta = $broj_predmeta + intval(mysql_result(myquery("SELECT sati_predavanja FROM predmet WHERE id = $pred_id"),0,0));
+					break;
+				case "vjezbe":
+					$broj_vjezbi_tutorijala = $broj_vjezbi_tutorijala + intval(mysql_result(myquery("SELECT sati_vjezbi FROM predmet WHERE id = $pred_id"),0,0));
+					break;
+				case "tutorijali":
+					$broj_vjezbi_tutorijala = $broj_vjezbi_tutorijala + intval(mysql_result(myquery("SELECT sati_tutorijala FROM predmet WHERE id = $pred_id"),0,0));
+					break;
+				case "vjezbe+tutorijali":
+					$broj_vjezbi_tutorijala = $broj_vjezbi_tutorijala + 2 * intval(mysql_result(myquery("SELECT sati_tutorijala FROM predmet WHERE id = $pred_id"),0,0));
+					break;
+			}
+		}
+		$predavanja_norma = $broj_predmeta / 12;
 		$vjezbe_tutorijali_norma = $broj_vjezbi_tutorijala / 12;
 		$norma = $predavanja_norma + $vjezbe_tutorijali_norma;
 	}
@@ -101,11 +162,11 @@ $default_vrijednosti=myquery("SELECT * FROM defaultne_vrijednosti_plate"); //def
 	$zaposljavanje_na_teret_osiguranja = mysql_result($default_vrijednosti, 0, 5) * 100;//0.015;       //unosi se, ostaju default vrijednosti
 	$neto_placa_sa_porezom = (1 - $penziono_i_invalidno_osiguranje - $zdravstveno_osiguranje - $zaposljavanje_na_teret_osiguranja) * $bruto_placa;
 	$porezna_olaksica = 0;       //unosi se
-	$porez_na_placu = 0.1;
+	$porez_na_placu = mysql_result($default_vrijednosti, 0, 6);//0.1;
 	$POR = ($neto_placa_sa_porezom - $porezna_olaksica) * $porez_na_placu;
 	$NBP = $neto_placa_sa_porezom - $POR;
-	$broj_radnih_dana_u_mjesecu = mysql_result($default_vrijednosti, 0, 6);//22;       //unosi se
-	$dnevni_topli_obrok = mysql_result($default_vrijednosti, 0, 7);//16;       //unosi se
+	$broj_radnih_dana_u_mjesecu = mysql_result($default_vrijednosti, 0, 7);//22;       //unosi se
+	$dnevni_topli_obrok = mysql_result($default_vrijednosti, 0, 8);//16;       //unosi se
 	$topli_obrok = $broj_radnih_dana_u_mjesecu * $dnevni_topli_obrok;
 	$ukupno_za_isplatu = $NBP + $topli_obrok;
 	
@@ -159,6 +220,7 @@ $default_vrijednosti=myquery("SELECT * FROM defaultne_vrijednosti_plate"); //def
 <?
 	}
 	else if($subakcija == "izracunaj"){
+	$default_vrijednosti=myquery("SELECT * FROM defaultne_vrijednosti_plate"); //defaultne vrijednosti varijabli potrebnih za racunanje plate, kao sto su porezi, broj radnih dana, itd.
 		$ime_prezime = myquery("SELECT ime, prezime FROM osoba WHERE id=$osoba");
 		$ime = mysql_result($ime_prezime,0,0);
 		$prezime = mysql_result($ime_prezime,0,1);
@@ -187,7 +249,7 @@ $default_vrijednosti=myquery("SELECT * FROM defaultne_vrijednosti_plate"); //def
 		$porezna_olaksica = $_POST['porezna_olaksica'];
 		if($porezna_olaksica == null or $porezna_olaksica == "")
 			$porezna_olaksica = 0;
-		$porez_na_placu = 0.1;
+		$porez_na_placu = mysql_result($default_vrijednosti, 0, 6);
 		$POR = round(($neto_placa_sa_porezom - $porezna_olaksica) * $porez_na_placu, 3);
 		$NBP = round($neto_placa_sa_porezom - $POR, 3);
 		$broj_radnih_dana_u_mjesecu = $_POST['broj_radnih_dana_u_mjesecu'];
