aabb52ba5a7b9cbce6681cda041a0d5cd2b21d7d
HumaMilisic <huma.milisic@gmail.com>
2015-05-03 21:55:31 +0200

    eh

--- tests/acceptance/SigninCept.php
+++ tests/acceptance/SigninCept.php
@@ -3,8 +3,9 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('perform actions and see result');
 $I->wantTo('login kao administrator');
 $I->amOnPage('/');
+$I->see("bolognaware");
 $I->fillField('login', 'admin');
 $I->fillField('pass', 'admin');
 $I->click("Kreni");
-$I->see('Administracija sajta');
+$I->see('Administracija sajta')
 ?>
