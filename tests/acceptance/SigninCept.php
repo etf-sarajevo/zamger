62f736519bb5337dce8ffdb88d5a8ac86cbde270
HumaMilisic <huma.milisic@gmail.com>
2015-05-03 19:38:50 +0200

    test jedan bez baze Signed-off-by:HumaMilisic <huma.milisic@gmail.com>

--- tests/acceptance/SigninCept.php
+++ tests/acceptance/SigninCept.php
@@ -1,4 +1,10 @@
 <?php 
 $I = new AcceptanceTester($scenario);
 $I->wantTo('perform actions and see result');
+$I->wantTo('login kao administrator');
+$I->amOnPage('/');
+$I->fillField('login', 'admin');
+$I->fillField('pass', 'admin');
+$I->click("Kreni");
+$I->see('Administracija sajta');
 ?>
