36b832d6d29b3ed46eb5e7abe07a294e1bc850a3
Huma <huma.milisic@gmail.com>
2015-06-07 19:11:49 +0200

    sauce postavke Signed-off-by:Huma <huma.milisic@gmail.com>

--- tests/acceptance/_bootstrap.php
+++ tests/acceptance/_bootstrap.php
@@ -2,18 +2,22 @@
 // Here you can initialize variables that will be available to your tests
 
 \Codeception\Util\Autoload::registerSuffix('Steps', __DIR__.DIRECTORY_SEPARATOR.'_steps');
-
-\Codeception\Configuration::$defaultSuiteSettings['modules']['config']=[
-    'WebDriver' => [
-        'url' => 'http://localhost/index.php',
-        'host' => getenv('SELENIUM_HOST'),
-        'port' => 80,
-        'browser' => 'chrome',
-        'window_size' => '1024x768',
-        'wait' => 10,
-        'restart' =>false
-    ]
-];
+//
+//seleniumHost => getenv('SELENIUM_HOST'); 
+//\Codeception\Configuration::$defaultSuiteSettings['modules']['enabled'] = [
+//    'WebDriver'
+//];
+//\Codeception\Configuration::$defaultSuiteSettings['modules']['config']=[
+//    'WebDriver' => [
+//        'url' => 'http://localhost/index.php',
+//        'host' => getenv('SELENIUM_HOST'),
+//        'port' => 80,
+//        'browser' => 'chrome',
+//        'window_size' => '1024x768',
+//        'wait' => 10,
+//        'restart' =>false
+//    ]
+//];
 
 //WebDriver:
 //            url: 'http://localhost/index.php'
