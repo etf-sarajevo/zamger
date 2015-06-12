71cfc7d226c0a6420001c69164432c1b260f4d69
Huma <huma.milisic@gmail.com>
2015-06-12 23:05:59 +0200

    sauce connect Signed-off-by:Huma <huma.milisic@gmail.com>

--- tests/_support/AcceptanceHelper.php
+++ tests/_support/AcceptanceHelper.php
@@ -8,14 +8,6 @@ namespace Codeception\Module;
 
 class AcceptanceHelper extends \Codeception\Module
 {
-    protected $sauceApi;
-    
-//    public function &_sauceApi (){
-//        if (!($this->sauceApi instanceof \Sauce\Sausage\SauceAPI)) {
-//            $this->sauceApi = new \Sauce\Sausage\SauceAPI($this->config['username'], $this->config['accesskey']);
-//        }
-//        return $this->sauceApi;
-//    }
 
     public function _initialize() {
         $wd = $this->getModule('WebDriver');
@@ -26,42 +18,5 @@ class AcceptanceHelper extends \Codeception\Module
             'capabilities'=>$cap));
         
     }
-    
-     // HOOK: before each suite
-    public function _beforeSuite($settings = array()) {
-    }
-
-    // HOOK: after suite
-    public function _afterSuite() {
-    }    
-
-    // HOOK: before each step
-    public function _beforeStep(\Codeception\Step $step) {
-    }
-
-    // HOOK: after each step
-    public function _afterStep(\Codeception\Step $step) {
-    }
-
-    // HOOK: before test
-    public function _before(\Codeception\TestCase $test) {
-//        $ime = $test->getName();
-//        $wd = $this->getModule('WebDriver');
-//        $cap= array('name'=>  $ime);
-//        $wd->_reconfigure(array(
-//            'capabilities'=>$cap));
-    }
-
-    // HOOK: after test
-    public function _after(\Codeception\TestCase $test) {
-    }
-
-    // HOOK: on fail
-    public function _failed(\Codeception\TestCase $test, $fail) {
-        //$ime = $test->getName();
-        $wd = $this->getModule('WebDriver');
-        $wd->_reconfigure(array(
-            'passed'=>false));
-    }      
     
 }
--- tests/acceptance/_bootstrap.php
+++ tests/acceptance/_bootstrap.php
@@ -2,38 +2,3 @@
 // Here you can initialize variables that will be available to your tests
 
 \Codeception\Util\Autoload::registerSuffix('Steps', __DIR__.DIRECTORY_SEPARATOR.'_steps');
-//
-//seleniumHost => getenv('SELENIUM_HOST'); 
-//\Codeception\Configuration::$defaultSuiteSettings['modules']['enabled'] = [
-//    'WebDriver'
-//];
-//\Codeception\Configuration::$defaultSuiteSettings['modules']['config']=[
-//    'WebDriver' => [
-//        'url' => 'http://localhost/index.php',
-//        'host' => getenv('SELENIUM_HOST'),
-//        'port' => 80,
-//        'browser' => 'chrome',
-//        'window_size' => '1024x768',
-//        'wait' => 10,
-//        'restart' =>false
-//    ]
-//];
-
-//\Codeception\Configuration::
-
-//env:
-//    sauce:
-//        modules:
-//            enabled:
-//                - WebDriver
-//            config:
-//                WebDriver:
-//                    url: 'http://localhost/zamger/'
-//                    port: 80
-//                    browser: chrome
-//                    window_size: 1024x768
-//                    wait: 10
-//                    restart: false
-//                    capabilities:
-//                        unexpectedAlertBehaviour: 'accept'
-//                        platform: 'Linux'
