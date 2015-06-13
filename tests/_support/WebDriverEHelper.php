26b39750b27778a37030cbbd1b42f0dcd7599a90
Huma <huma.milisic@gmail.com>
2015-06-13 20:34:37 +0200

    rrwar

--- tests/_support/WebDriverEHelper.php
+++ tests/_support/WebDriverEHelper.php
@@ -6,6 +6,19 @@ namespace Codeception\Module;
 
 class WebDriverEHelper extends \Codeception\Module\WebDriver
 {
+    public function _after(\Codeception\TestCase $test)
+    {
+        if ($this->config['restart'] && isset($this->webDriver)) {
+            $this->webDriver->quit();
+            // \RemoteWebDriver consists of four parts, executor, mouse, keyboard and touch, quit only set executor to null,
+            // but \RemoteWebDriver doesn't provide public access to check on executor
+            // so we need to unset $this->webDriver here to shut it down completely
+//            $this->webDriver = null;
+        }
+        if ($this->config['clear_cookies'] && isset($this->webDriver)) {
+            $this->webDriver->manage()->deleteAllCookies();
+        }
+    }
 //    public function _after(\Codeception\TestCase $test) {
 //        if ($this->config['restart'] && isset($this->webDriver)) {
 //            $this->webDriver->quit();
