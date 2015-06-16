9a5f5de96fabb21979093d7033c376fd51165206
Huma <huma.milisic@gmail.com>
2015-06-16 21:24:31 +0200

    testovi
    sve preko ui

--- tests/acceptance/dummyCest.php
+++ tests/acceptance/dummyCest.php
@@ -4,14 +4,14 @@ use \AcceptanceTester;
 class dummyCest
 {
 //    public function _before(AcceptanceTester\fixturesStudentSteps $I)
-    {
-//        $I = new AcceptanceTester\MemberSteps($scenario);
-//        $I->randomNOsoba(1);
-    }
+//    {
+////        $I = new AcceptanceTester\MemberSteps($scenario);
+////        $I->randomNOsoba(1);
+//    }
 
 //    public function _after(AcceptanceTester\fixturesStudentSteps $I)
-    {
-    }
+//    {
+//    }
 
     // tests
     public function tryToTest(AcceptanceTester $I)
