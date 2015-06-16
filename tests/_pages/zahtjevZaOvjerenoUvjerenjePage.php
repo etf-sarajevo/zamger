c45ed2a483245d13cb69beda630877490addb09b
Huma <huma.milisic@gmail.com>
2015-06-16 09:21:11 +0200

    testovi
    konekcija s bazom

---
+++ tests/_pages/zahtjevZaOvjerenoUvjerenjePage.php
@@ -0,0 +1,26 @@
+<?php
+//class studentHomePage extends basePage
+class zahtjevZaOvjerenoUvjerenjePage extends studentHomePage
+{
+    // include url of current page
+    public static $URL;
+    public static $tipPotvrde;
+
+    /**
+     * Declare UI map for this page here. CSS or XPath allowed.
+     * public static $usernameField = '#username';
+     * public static $formSubmitButton = "#mainForm input[type=submit]";
+     */
+
+    /**
+     * Basic route example for your current URL
+     * You can append any additional parameter to URL
+     * and use it in tests like: EditPage::route('/123-post');
+     */
+    public static function route($param)
+    {
+        return static::$URL.$param;
+    }
+
+
+}
