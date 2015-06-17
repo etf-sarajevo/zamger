5152087eb3e4ef8e5734cbdfe5dc3abf2e3a0607
Huma <huma.milisic@gmail.com>
2015-06-17 22:24:53 +0200

    test

--- tests/acceptance/dumdumCept.php
+++ tests/acceptance/dumdumCept.php
@@ -1,7 +1,24 @@
 <?php 
 $I = new AcceptanceTester\MemberSteps($scenario);
 $I->loginKaoAdmin();
-$I->adminDodajPredmetKursSvima('predmet23', "!r@#", 5, 
-        5, 5, 
-        5, 2, 2, true);
+$I->click("Studentska služba");
+$I->click("Kreiranje plana studija");
+$I->see("Dodavanje stavke nastavnog plana");
+$I->selectOption("select[id=studij]", "4.Telekomunikacije (BSc)");
+$I->fillField("input[name=semestar]", 7);
+$k = 1;
+do {
+    $I->selectOption("select[id=predmet]", $k);
+    $temp = $I->grabValueFrom();
+            //"select[id=predmet]");
+    $I->canSeeElementInDOM($selector);
+//    $I->gra
+    $tempString = explode(".", $temp);
+    if($tempString[1]=="dummyPredmet"){    
+        break;        
+    }
+    $k++;
+}while(true);
+
+$I->click('input[id=submitbutton]');
 $I->logout();
