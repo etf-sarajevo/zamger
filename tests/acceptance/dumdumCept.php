f3ea2d4eae37a8887f57e2ff0b2727c5cd1f2491
Huma <huma.milisic@gmail.com>
2015-06-19 09:30:17 +0200

    test
    env varijable

--- tests/acceptance/dumdumCept.php
+++ tests/acceptance/dumdumCept.php
@@ -1,24 +1,30 @@
 <?php 
 $I = new AcceptanceTester\MemberSteps($scenario);
-$I->loginKaoAdmin();
-$I->click("Studentska služba");
-$I->click("Kreiranje plana studija");
-$I->see("Dodavanje stavke nastavnog plana");
-$I->selectOption("select[id=studij]", "4.Telekomunikacije (BSc)");
-$I->fillField("input[name=semestar]", 7);
-$k = 1;
-do {
-    $I->selectOption("select[id=predmet]", $k);
-    $temp = $I->grabValueFrom();
-            //"select[id=predmet]");
-    $I->canSeeElementInDOM($selector);
-//    $I->gra
-    $tempString = explode(".", $temp);
-    if($tempString[1]=="dummyPredmet"){    
-        break;        
-    }
-    $k++;
-}while(true);
-
-$I->click('input[id=submitbutton]');
-$I->logout();
+//$I->loginKaoAdmin();
+//$I->click("Studentska služba");
+//$I->click("Kreiranje plana studija");
+//$I->see("Dodavanje stavke nastavnog plana");
+//$I->selectOption("select[id=studij]", "4.Telekomunikacije (BSc)");
+//$I->fillField("input[name=semestar]", 7);
+//$I->executeInSelenium($function);
+////$k = 1;
+////do {
+////    $I->selectOption("select[id=predmet]", $k);
+////    $temp = $I->grabValueFrom();
+////            //"select[id=predmet]");
+////    $I->canSeeElementInDOM($selector);
+//////    $I->gra
+////    $tempString = explode(".", $temp);
+////    if($tempString[1]=="dummyPredmet"){    
+////        break;        
+////    }
+////    $k++;
+////}while(true);
+//
+//$I->executeInSelenium(function(RemoteWebDriver $webDriver) {
+//    $webDriver->getKeyboard()->pressKey(WebDriverKeys::ENTER);
+//    $webDriver->findElements("option");
+////    $webDriver->
+//});
+//$I->click('input[id=submitbutton]');
+//$I->logout();
