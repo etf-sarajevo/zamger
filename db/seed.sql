1eb9b35bc6625782184677341ce4ca01a01ab316
Vedran Ljubovic <vljubovic@smartnet.ba>
2016-06-20 09:08:20 +0200

    Šema baze je ažurirana

--- db/seed.sql
+++ db/seed.sql
@@ -745,7 +745,7 @@ INSERT INTO `posebne_kategorije` (`id`, `naziv`) VALUES
 --
 
 INSERT INTO `preference` (`korisnik`, `preferenca`, `vrijednost`) VALUES
-(0, 'verzija-baze', '1465985289');
+(0, 'verzija-baze', '1466406454');
 
 
 --
