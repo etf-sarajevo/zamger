b3be367e4579a571d21e9f45a9d98e967b736a7b
Vedran Ljubovic <vljubovic@smartnet.ba>
2017-03-14 10:41:07 +0100

    Popravljam još linkova na static/

--- index.php
+++ index.php
@@ -571,7 +571,7 @@ else
 
 <p>&nbsp;</p>
 <p>&nbsp;</p>
-<p align="center">Copyright (c) 2006-2017 <a href="doc/CREDITS.txt">Vedran Ljubović i drugi</a><br>Elektrotehnički fakultet Sarajevo</p>
+<p align="center">Copyright (c) 2006-2017 <a href="static/doc/CREDITS.txt">Vedran Ljubović i drugi</a><br>Elektrotehnički fakultet Sarajevo</p>
 
 </body>
 </html>
