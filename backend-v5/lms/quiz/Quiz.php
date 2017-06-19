df607924d6ec2754223ab61bc3f3d94f0c5d4953
Vedran Ljubovic <vljubovic@smartnet.ba>
2017-06-19 10:51:21 +0200

    More support for taking quiz in API

--- backend-v5/lms/quiz/Quiz.php
+++ backend-v5/lms/quiz/Quiz.php
@@ -13,6 +13,8 @@ class Quiz {
 	public $id;
 	public $name, $CourseUnit, $AcademicYear, $Group, $timeBegin, $timeEnd, $active, $ipAddressRanges, $passPoints, $nrQuestions, $duration; /* in seconds */
 	// $zclassId -- dodati link na čas umjesto kako je sada, link sa časa na kviz
+
+	const $tokenLength = 20; // Lenght of quiz token
 	
 	public static function fromId($id) {
 		$quiz = DB::query_assoc("SELECT id, naziv name, predmet CourseUnit, akademska_godina AcademicYear, labgrupa _Group, UNIX_TIMESTAMP(vrijeme_pocetak) timeBegin, UNIX_TIMESTAMP(vrijeme_kraj) timeEnd, aktivan active, ip_adrese ipAddressRanges, prolaz_bodova passPoints, broj_pitanja nrQuestions, trajanje_kviza duration FROM kviz WHERE id=$id");
@@ -26,22 +28,79 @@ class Quiz {
 	}
 	
 	// Allow student to take quiz
-	public static function take($studentId, $id) {
+	public static function take($studentId, $quizId) {
 		try {
 			// If student never took this quiz, below will throw an exception
-			$qr = QuizResult::fromStudentAndQuiz($studentId, $id);
+			$qr = QuizResult::fromStudentAndQuiz($studentId, $quizId);
 			throw new Exception("Quiz already taken", "703");
 		} catch(Exception $e) {
 			// Proceed to taking quiz
 		}
 		
-		$quiz = DB::query_assoc("SELECT id, naziv name, predmet CourseUnit, akademska_godina AcademicYear, labgrupa _Group, UNIX_TIMESTAMP(vrijeme_pocetak) timeBegin, UNIX_TIMESTAMP(vrijeme_kraj) timeEnd, aktivan active, ip_adrese ipAddressRanges, prolaz_bodova passPoints, broj_pitanja nrQuestions, trajanje_kviza duration FROM kviz WHERE id=$id");
-		if (!$quiz) throw new Exception("Unknown quiz $id", "404");
+		$quiz = DB::query_assoc("SELECT id, naziv name, predmet CourseUnit, akademska_godina AcademicYear, labgrupa _Group, UNIX_TIMESTAMP(vrijeme_pocetak) timeBegin, UNIX_TIMESTAMP(vrijeme_kraj) timeEnd, aktivan active, ip_adrese ipAddressRanges, prolaz_bodova passPoints, broj_pitanja nrQuestions, trajanje_kviza duration FROM kviz WHERE id=$quizId");
+		if (!$quiz) throw new Exception("Unknown quiz $quizId", "404");
 		
 		$quiz['Group'] = $quiz['_Group']; unset($quiz['_Group']); // SQL reserved word
 		$quiz = Util::array_to_class($quiz, "Quiz", array("CourseUnit", "AcademicYear", "Group"));
 		if ($quiz->active == 1) $quiz->active=true; else $quiz->active=false; // FIXME use boolean in database
 		$quiz->questions = QuizQuestion::takeQuiz($id, $quiz->nrQuestions, true);
+
+		// Create a QuizResult object with finished=false
+		$quiz->result = QuizResult::create($studentId, $quizId);
+
+		return $quiz;
+	}
+
+	
+	// Student submits quiz results by calling submit method below on Quiz object
+	// It's assumed that object is returned by Quiz::take() and that QuizAnswer objects
+	// which user thinks are correct are filled with the attribute QuizAnswer::correct=true
+	public function submit($studentId) {
+		// First check if there's QuizResult with finished=false
+		try {
+			$qr = QuizResult::fromStudentAndQuiz($studentId, $id);
+			if ($qr->finished)
+				throw new Exception("Quiz already taken", "703");
+		} catch(Exception $e) {
+			throw new Exception("Student $studentId never started quiz " . $this->id, "404");
+		}
+
+		// Next check token
+		if ($this->result->token != $qr->token)
+			throw new Exception("Invalid token", "403");
+
+		// Since this object may come from user, the only info we implicitely trust is 
+		// quiz id and token
+		// So we get a brand new quiz object
+		$quiz = Quiz::fromId($this->id);
+		if (time() > $quiz->timeEnd)
+			throw new Exception("Sorry, quiz " . $quiz->id . " has expired", "403");
+		if (time() - $qr->timeActivated > $quiz->duration)
+			throw new Exception("You took too long to finish quiz " . $quiz->id, "403");
+			
+		// Resolve questions and answers
+		$score = 0;
+		foreach ($quiz->questions as &$qq) {
+			$qq->resolve();
+			$correct_answer = 0;
+			foreach($qq->answers as &$qa) {
+				$qa->resolve();
+				if ($qa->correct) $correct_answer = $qa->id;
+			}
+			
+			// Find matching question/answer in user submitted data
+			foreach($this->questions as $quq)
+				if ($quq->id == $qq->id)
+					foreach($quq->answers as $qua)
+						if ($qua->correct && $qua->id == $correct_answer) $score++;
+		}
+		
+		// Set score and finished status for QuizResult
+		$qr->finished = true;
+		$qr->score = $score;
+		$qr->update();
+		$quiz->result = $qr;
+		
 		return $quiz;
 	}
 	
--- backend-v5/lms/quiz/QuizResult.php
+++ backend-v5/lms/quiz/QuizResult.php
@@ -6,10 +6,10 @@
 
 
 class QuizResult {
-	public $student, $Quiz, $finished, $score, $timeActivated;
+	public $student, $Quiz, $finished, $score, $timeActivated, $token;
 	
 	public static function fromStudentAndQuiz($studentId, $quizId) {
-		$qr = DB::query_assoc("SELECT student, kviz Quiz, dovrsen finished, bodova score, UNIX_TIMESTAMP(vrijeme_aktivacije) timeActivated FROM kviz_student WHERE student=$studentId AND kviz=$quizId");
+		$qr = DB::query_assoc("SELECT student, kviz Quiz, dovrsen finished, bodova score, UNIX_TIMESTAMP(vrijeme_aktivacije) timeActivated, token FROM kviz_student WHERE student=$studentId AND kviz=$quizId");
 		if (!$qr) throw new Exception("Student $studentId never took quiz $quizId", "404");
 		
 		$qr = Util::array_to_class($qr, "QuizResult", array("Quiz"));
@@ -18,14 +18,37 @@ class QuizResult {
 		return $qr;
 	}
 	
-	public function update() {
-		if ($this->finished) $finsql="1"; else $finsql="0";
-		DB::query("UPDATE kviz_student SET dovrsen=$finsql, bodova=".$this->score.", vrijeme_aktivacije=FROM_UNIXTIME(".$this->timeActivated.") WHERE student=".$this->studentId." and kviz=".$this->quizId);
+	// Creates new QuizResult, sets finished status to false (student started taking the quiz)
+	public static function create($studentId, $quizId) {
+		$qr = new QuizResult;
+		$qr->student = new UnresolvedClass("Person", $studentId, $qr->student);
+		$qr->Quiz = new UnresolvedClass("Quiz", $quizId, $qr->Quiz);
+		$qr->finished = false;
+		$qr->score = 0;
+
+		// Find a unique token
+		do {
+			$characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+			$token = "";
+			for ($i=0; $i<Quiz::$tokenLength; $i++) 
+				$token .= $characters[rand(0,strlen($characters))];
+			$token_exists = DB::query("SELECT COUNT(*) FROM kviz_student WHERE token='$token'");
+		} while ($token_exists > 0);
+
+		$qr->token = $token;
+		$qr->add();
+		return $qr;
 	}
 	
-	public function add() {
+	public function update() {
 		if ($this->finished) $finsql="1"; else $finsql="0";
-		DB::query("INSERT INTO kviz_student SET student=".$this->studentId.", kviz=".$this->quizId.", dovrsen=$finsql, bodova=".$this->score.", vrijeme_aktivacije=NOW()");
+		DB::query("UPDATE kviz_student SET dovrsen=$finsql, bodova=".$this->score.", vrijeme_aktivacije=FROM_UNIXTIME(".$this->timeActivated."), token='".$this->token."' WHERE student=".$this->studentId." and kviz=".$this->quizId);
+	}
+
+	// Add method is private - we want others to use QuizResult::create()
+	private function add() {
+		if ($this->finished) $finsql="1"; else $finsql="0";
+		DB::query("INSERT INTO kviz_student SET student=".$this->studentId.", kviz=".$this->quizId.", dovrsen=$finsql, bodova=".$this->score.", vrijeme_aktivacije=NOW(), token='".$this->token."'");
 	}
 	
 	public function delete() {
