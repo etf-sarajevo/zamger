bea7bdcae7858fe90df2974b15ded2704e4c46ec
vljubovic <vljubovic@smartnet.ba>
2018-03-06 11:05:05 +0100

    Uvijek generiši statičke izvještaje sa razdvojenim ispitima

--- nastavnik/ocjena.php
+++ nastavnik/ocjena.php
@@ -237,7 +237,7 @@ if ($_POST['akcija'] == "massinput" && strlen($_POST['nazad'])<1 && check_csrf_t
 		return;
 	} else {
 		// Generisem statičku verziju izvještaja predmet
-		generisi_izvjestaj_predmet( $predmet, $ag, array("skrati" => "da", "sakrij_imena" => "da") );
+		generisi_izvjestaj_predmet( $predmet, $ag, array("skrati" => "da", "sakrij_imena" => "da", "razdvoji_ispite" => "da") );
 
 		zamgerlog("masovno upisane ocjene na predmet pp$predmet",4);
 		
