dba11df2a543a67f5d02286a12fb4301c441de8c
Vedran Ljubović <vljubovic@gmail.com>
2019-05-07 20:40:25 +0200

    public/ical: Striktniji SQL u novoj verziji MySQLa

--- public/ical.php
+++ public/ical.php
@@ -66,7 +66,7 @@ function public_ical() {
 		$sqlUpit = "SELECT rs.id, p.naziv, p.kratki_naziv, rs.dan_u_sedmici, rs.tip, rs.vrijeme_pocetak, rs.vrijeme_kraj, rs.labgrupa, rsala.naziv, rs.fini_pocetak, rs.fini_kraj, UNIX_TIMESTAMP(r.vrijeme_kreiranja)
 		FROM raspored_stavka as rs, raspored as r, predmet as p, ponudakursa as pk, student_predmet as sp, student_labgrupa as sl, raspored_sala as rsala, akademska_godina as ag
 		WHERE sp.student=$userid AND sp.predmet=pk.id AND pk.predmet=p.id AND pk.akademska_godina=ag.id and pk.semestar mod 2=$neparni and ag.aktuelna=1 AND p.id=rs.predmet AND rs.raspored=r.id AND r.aktivan=1 AND sl.student=$userid AND (rs.labgrupa=0 or rs.labgrupa=sl.labgrupa) AND rs.sala=rsala.id
-		GROUP BY rs.labgrupa, rs.dan_u_sedmici, rs.vrijeme_pocetak, p.naziv
+		GROUP BY rs.id, rs.labgrupa, rs.dan_u_sedmici, rs.vrijeme_pocetak, p.naziv
 		ORDER BY rs.dan_u_sedmici ASC, rs.vrijeme_pocetak ASC, rs.id ASC";
 
 	}
