343a7d48e1e4810fb2fce356c6ba0410d53b0204
vljubovic <vljubovic@smartnet.ba>
2020-01-31 10:45:55 +0100

    studentska/priznavanje: Datum i broj protokola su fiksni za sve ocjene
    Osim toga: reformatiranje, ispravke sitnih greškica, notifikacija da su podaci uspješno uneseni

--- studentska/priznavanje.php
+++ studentska/priznavanje.php
@@ -1,6 +1,90 @@
 <?php
 function studentska_priznavanje(){
-    
+
+	$akcija = $_POST['akcija'];
+	if($akcija == "dodaj"){
+		// db_escape()
+		$student = db_escape($_POST["student"]);
+		$akademska_godina = db_escape($_POST["ag"]);
+		$ciklus = db_escape($_POST["ciklus"]);
+		$strana_institucija = db_escape($_POST["strana_institucija"]);
+		$broj_protokola = db_escape($_POST["broj_protokola"]);
+		$datum = db_escape($_POST["datum"]);
+		
+		if($akademska_godina == -1) {
+			niceerror("Nije izabrana akademska godina.");
+			return;
+		}
+	
+		if(!$broj_protokola) {
+			niceerror("Morate unijeti broj protokola.");
+		}
+
+		if(!$strana_institucija) {
+			niceerror("Morate unijeti stranu instituciju.");
+		}
+		
+		// treba nam broj unesenih predmeta
+		// za svaki cemo posebno query slat
+		$broj_predmeta = db_escape($_POST["kolicina"]);
+
+		$index=end(explode(" ", $student));
+		$index=str_replace("(","",$index);
+		$index=str_replace(")","",$index);
+		if(!$index){
+			niceerror("Nije izabran student!");
+			return;
+		}
+
+		$query = db_query("select id from osoba where brindexa=\"$index\"");
+		$student_id = db_fetch_row($query)[0];
+		
+		$make_odluka = db_query("insert into odluka set datum='$datum', broj_protokola='$broj_protokola', student=$student_id");
+		$odluka_id = db_insert_id('odluka', 'id');
+
+		for ($i=1; $i <= $broj_predmeta; $i++) { 
+		$naziv_predmeta = db_escape($_POST["naziv_predmeta".$i]);
+		$sifra_predmeta = db_escape($_POST["sifra_predmeta".$i]);
+		$ects = db_escape($_POST["ects".$i]);
+		$ocjena = db_escape($_POST["ocjena".$i]);
+
+		// Form validation
+
+		if(!$naziv_predmeta) {
+			niceerror("Morate unijeti naziv predmeta.");
+			return;
+		}
+
+		if(!$sifra_predmeta){
+			niceerror("Morate unijeti sifru premdeta!");
+			return;
+		}
+
+		$ects = floatval($ects);
+		if($ects <= 0){
+			niceerror("ECTS krediti moraju biti pozitivni!");
+			return;
+		}
+
+		if($ocjena < 6 || $ocjena > 10) {
+			niceerror("Ocjena mora biti između 6 i 10");
+			return;
+		}
+
+
+		$priznavanje = db_query("insert into priznavanje set 
+			student=$student_id, 
+			akademska_godina=$akademska_godina,
+			ciklus=$ciklus, 
+			naziv_predmeta='$naziv_predmeta', 
+			sifra_predmeta='$sifra_predmeta', 
+			ects=$ects, ocjena=$ocjena, odluka=$odluka_id, 
+			strana_institucija='$strana_institucija'");
+		}
+		
+		nicemessage("Registrovano je priznavanje ocjena");
+		return;
+	}
 ?>
 
 <center>
@@ -59,7 +143,18 @@ function studentska_priznavanje(){
         <td><label for='strana_institucija'>Strana institucija: </label></td>
         <td><input type='text' name='strana_institucija'/></td>
     </tr>
-    
+
+    <tr>
+        <td><label for='broj_protokola'>Broj protokola: </label></td>
+        <td><input type='text' name='broj_protokola1'/></td>
+    </tr>
+
+
+    <tr >
+        <td><label for='datum'>Datum: </label></td>
+        <td><input type='date' value="<?= date('Y-m-j')?>" name='datum1' /></td>
+    </tr>
+
     <tr>    <td colspan="2">    <hr>    </td></tr>
     <tr >
         <td ><label for='naziv_predmeta1'>Naziv predmeta: </label></td>
@@ -80,17 +175,6 @@ function studentska_priznavanje(){
         <td><label for='ocjena1'>Ocjena: </label></td>
         <td><input type='number' min='6' max='10' name='ocjena1'/></td>
     </tr>
-
-    <tr>
-        <td><label for='broj_protokola1'>Broj protokola: </label></td>
-        <td><input type='text' name='broj_protokola1'/></td>
-    </tr>
-
-         
-    <tr >
-        <td><label for='datum1'>Datum: </label></td>
-        <td><input type='date' value="<?= date('Y-m-j')?>" name='datum1' /></td>
-    </tr>
     <tr id="anchor" ><td><input type="hidden" id="kolicina" name="kolicina" value='1'></td></tr>
     <tr>
         <td><input value="Dodaj predmet" type="button" onclick="dodaj_predmet()"></td>
@@ -101,138 +185,48 @@ function studentska_priznavanje(){
 </form> 
 </center>
 <script>
-    function dodaj_predmet(){
-        let node = document.getElementById('kolicina');
-        let kolicina = Number(node.value);
-        kolicina = kolicina + 1; // koji je po redu
-        node.value = kolicina; // update for next
+	function dodaj_predmet(){
+		let node = document.getElementById('kolicina');
+		let kolicina = Number(node.value);
+		kolicina = kolicina + 1; // koji je po redu
+		node.value = kolicina; // update for next
 
-        let naziv_predmeta = document.createElement('tr');
-        naziv_predmeta.innerHTML = `
-            <td ><label for='naziv_predmeta${kolicina}'>Naziv predmeta: </label></td>
-            <td><input type='text' name='naziv_predmeta${kolicina}'/></td>`;
-        
-        let sifra_predmeta = document.createElement('tr');
-        sifra_predmeta.innerHTML = `
-            <td><label for='sifra_predmeta${kolicina}'>Šifra predmeta: </label></td>
-            <td><input type='text' name='sifra_predmeta${kolicina}'/></td>`;
+		let naziv_predmeta = document.createElement('tr');
+		naziv_predmeta.innerHTML = `
+		<td ><label for='naziv_predmeta${kolicina}'>Naziv predmeta: </label></td>
+		<td><input type='text' name='naziv_predmeta${kolicina}'/></td>`;
 
-        let ects = document.createElement('tr');
-        ects.innerHTML = `
-            <td><label for='ects${kolicina}'>ECTS: </label></td>
-            <td><input type='number' min='0' step='0.5' name='ects${kolicina}'/></td>`;
+		let sifra_predmeta = document.createElement('tr');
+		sifra_predmeta.innerHTML = `
+		<td><label for='sifra_predmeta${kolicina}'>Šifra predmeta: </label></td>
+		<td><input type='text' name='sifra_predmeta${kolicina}'/></td>`;
 
-        let ocjena = document.createElement('tr');
-        ocjena.innerHTML = `
-            <td><label for='ocjena${kolicina}'>Ocjena: </label></td>
-            <td><input type='number' min='6' max='10' name='ocjena${kolicina}'/></td>    `;
+		let ects = document.createElement('tr');
+		ects.innerHTML = `
+		<td><label for='ects${kolicina}'>ECTS: </label></td>
+		<td><input type='number' min='0' step='0.5' name='ects${kolicina}'/></td>`;
+
+		let ocjena = document.createElement('tr');
+		ocjena.innerHTML = `
+		<td><label for='ocjena${kolicina}'>Ocjena: </label></td>
+		<td><input type='number' min='6' max='10' name='ocjena${kolicina}'/></td>    `;
 
 
-        let broj_protokola = document.createElement('tr');
-        broj_protokola.innerHTML = `
-            <td><label for='broj_protokola${kolicina}'>Broj protokola: </label></td>
-        <td><input type='text' name='broj_protokola${kolicina}'/></td>`;
-
-        let datum = document.createElement('tr');
-        datum.innerHTML = `
-            <td><label for='datum${kolicina}'>Datum: </label></td>
-            <td><input type='date' value="<?= date('Y-m-j')?>" name='datum${kolicina}' /></td>`;
-        
-        let horizontal = document.createElement('tr');
-        horizontal.innerHTML = '<td colspan="2"><hr></td>';
-        let anchor = document.getElementById('anchor');
-        anchor.parentNode.insertBefore(horizontal,anchor);
-        anchor.parentNode.insertBefore(naziv_predmeta,anchor);
-        anchor.parentNode.insertBefore(sifra_predmeta,anchor);
-        anchor.parentNode.insertBefore(ects,anchor);
-        anchor.parentNode.insertBefore(ocjena,anchor);
-        anchor.parentNode.insertBefore(broj_protokola,anchor);
-        anchor.parentNode.insertBefore(datum,anchor);
-        return false;
-    }
+		let horizontal = document.createElement('tr');
+		horizontal.innerHTML = '<td colspan="2"><hr></td>';
+		let anchor = document.getElementById('anchor');
+		anchor.parentNode.insertBefore(horizontal,anchor);
+		anchor.parentNode.insertBefore(naziv_predmeta,anchor);
+		anchor.parentNode.insertBefore(sifra_predmeta,anchor);
+		anchor.parentNode.insertBefore(ects,anchor);
+		anchor.parentNode.insertBefore(ocjena,anchor);
+		anchor.parentNode.insertBefore(broj_protokola,anchor);
+		anchor.parentNode.insertBefore(datum,anchor);
+		return false;
+	}
 </script>
 <?php
 
+    
 }
-
-    $akcija = $_POST['akcija'];
-    if($akcija == "dodaj"){
-        // db_escape()
-        $student = db_escape($_POST["student"]);
-        $akademska_godina = db_escape($_POST["ag"]);
-        $ciklus = db_escape($_POST["ciklus"]);
-        $strana_institucija = db_escape($_POST["strana_institucija"]);
-        // treba nam broj unesenih predmeta
-        // za svaki cemo posebno query slat
-        $broj_predmeta = db_escape($_POST["kolicina"]);
-        
-        $index=end(explode(" ", $student));
-        $index=str_replace("(","",$index);
-        $index=str_replace(")","",$index);
-        if(!$index){
-            niceerror("Nije izabran student!");
-            return;
-        }
-        
-        $query = db_query("select id from osoba where brindexa=\"$index\"");
-        $student_id = db_fetch_row($query)[0];
-        $odluka_id = db_insert_id('odluka', 'id');
-
-        for ($i=1; $i <= $broj_predmeta; $i++) { 
-            $naziv_predmeta = db_escape($_POST["naziv_predmeta".$i]);
-            $sifra_predmeta = db_escape($_POST["sifra_predmeta".$i]);
-            $ects = db_escape($_POST["ects".$i]);
-            $ocjena = db_escape($_POST["ocjena".$i]);
-            $broj_protokola = db_escape($_POST["broj_protokola".$i]);
-            $datum = db_escape($_POST["datum".$i]);
-    
-            // Form validation
-    
-            if($akademska_godina == -1) {
-                niceerror("Nije izabrana akademska godina.");
-                return;
-            }
-            
-            if(!$naziv_predmeta) {
-                niceerror("Morate unijeti naziv predmeta.");
-                return;
-            }
-            
-            if(!$sifra_predmeta){
-                niceerror("Morate unijeti sifru premdeta!");
-                return;
-            }
-    
-            $ects = floatval($ects);
-            if($ects <= 0){
-                niceerror("ECTS krediti moraju biti pozitivni!");
-                return;
-            }
-    
-            if($ocjena < 6 || $ocjena > 10) {
-                niceerror("Ocjena mora biti između 6 i 10");
-                return;
-            }
-    
-            if(!$broj_protokola) {
-                niceerror("Morate unijeti broj protokola.");
-            }
-    
-            if(!$strana_institucija) {
-                niceerror("Morate unijeti stranu instituciju.");
-            }
-    
-            $make_odluka = db_query("insert into odluka set 
-                datum='$datum', broj_protokola='$broj_protokola', student=$student_id");
-    
-            $priznavanje = db_query("insert into priznavanje set 
-                student=$student_id, 
-                akademska_godina=$akademska_godina,
-                ciklus=$ciklus, 
-                naziv_predmeta='$naziv_predmeta', 
-                sifra_predmeta='$sifra_predmeta', 
-                ects=$ects, ocjena=$ocjena, odluka=$odluka_id, 
-                strana_institucija='$strana_institucija'");
-        }
-    }
 ?>
