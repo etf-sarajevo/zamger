65d79e3474120488f5200be7acd205afc9b66d98
Vedran Ljubović <vljubovic@smartnet.ba>
2020-02-02 18:41:17 +0100

    Improve CORS

--- api-v5/index.php
+++ api-v5/index.php
@@ -62,7 +62,8 @@ header("Access-Control-Allow-Credentials: true");
 
 // CORS pre-flight response is needed because we send application/json content-typeheader
 if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
-	header("Access-Control-Allow-Headers: Content-Type");
+	header("Access-Control-Allow-Headers: Content-Type,Set-Cookie,Accept");
+	header("Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT");
 	return;
 }
 
