0cfa9348cad09a2fe2e4252139766b632fc52286
Vedran Ljubović <vljubovic@smartnet.ba>
2022-02-21 20:28:42 +0100

    Implement new OAuth2Helper in get_token.php

--- get_token.php
+++ get_token.php
@@ -8,11 +8,12 @@ require_once("lib/dblayer.php");
 require_once("lib/zamger.php");
 require_once("lib/session.php"); // check_cookie
 require_once("lib/hack.php");
+require_once("lib/oauth2_client.php");
 
 db_connect($conf_dbhost,$conf_dbuser,$conf_dbpass,$conf_dbdb);
 check_cookie();
 if ($userid != 0) {
-	print "Token: " . get_keycloak_token();
+	print "Token: " . OAuth2Helper::getToken($login);
 } else {
 	header("HTTP/1.0 401 Unauthorized");
 }
